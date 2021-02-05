2975f3e6c149ef8fbf147f42837d46bb37c7ce51
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 12:29:29 +0000

    More exercices

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -1,8 +1,26 @@
 // Exercices from course https://www.youtube.com/watch?v=_bYFu9mBnr4
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 
+double power (int a, int b)
+{
+    double result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
 
+int main()
+{
+    int base, exponent;
+    cout << "What is the base? ";
+    cin >> base;
+    cout << "What is the exponent? ";
+    cin >> exponent;
+    double result = power(base, exponent);
+    cout << "The result is " << result;
+}
 
--- My Drafts.cpp
+++ My Drafts.cpp
@@ -32,3 +32,27 @@ int main()
     double result = pow(base, exponent);
     cout << "The result is " << result;
 }
+
+#include <iostream>
+using namespace std;
+
+double power (int a, int b)
+{
+    double result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int main()
+{
+    int base, exponent;
+    cout << "What is the base? ";
+    cin >> base;
+    cout << "What is the exponent? ";
+    cin >> exponent;
+    double result = power(base, exponent);
+    cout << "The result is " << result;
+}
