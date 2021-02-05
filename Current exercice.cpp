69308445dc657813292238e147be1ef9744c3aaa
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 12:22:16 +0000

    First exercices

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -1,10 +1,8 @@
 // Exercices from course https://www.youtube.com/watch?v=_bYFu9mBnr4
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    int slices = 10;
-    cout << "You Have " << slices << " of pizza" << endl;
-}
+
+
--- My Drafts.cpp
+++ My Drafts.cpp
@@ -7,5 +7,28 @@ using namespace std;
 int main()
 {
     int slices = 10;
-    cout << "You Have" << slices << "of pizza";
+    cout << "You Have " << slices << " of pizza";
+}
+
+int main()
+{
+    int slices;
+    cout << "How many slices of pizza you have now? ";
+    cin >> slices;
+    cout << "You Have " << slices << " of pizza" << endl;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int base, exponent;
+    cout << "What is the base? ";
+    cin >> base;
+    cout << "What is the exponent? ";
+    cin >> exponent;
+    double result = pow(base, exponent);
+    cout << "The result is " << result;
 }
