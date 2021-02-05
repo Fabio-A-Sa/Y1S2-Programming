d6801d32c99d725a518ab44abe245491b817f331
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 12:47:40 +0000

    Areas and volumes

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -3,24 +3,33 @@
 #include <iostream>
 using namespace std;
 
-double power (int a, int b)
+int area_superficie (int comprimento, int largura, int altura)
 {
-    double result = 1;
-    for (int i = 0; i < b; i++)
-    {
-        result = result * a;
-    }
-    return result;
+    int area; // Result
+    area = 2 * (comprimento * largura + comprimento * altura + largura * altura);
+    return area;
+}
+
+int volume_total (int comprimento, int largura, int altura)
+{
+    int volume; // Result
+    volume = comprimento * largura * altura;
+    return volume;
 }
 
 int main()
 {
-    int base, exponent;
-    cout << "What is the base? ";
-    cin >> base;
-    cout << "What is the exponent? ";
-    cin >> exponent;
-    double result = power(base, exponent);
-    cout << "The result is " << result;
-}
+    int l, w, h;
+    cout << "Imagin that you have a solid. What big is it?" << endl;
+    cout << "Lenght: ";
+    cin >> l;
+    cout << "Width: ";
+    cin >> w;
+    cout << "Height: ";
+    cin >> h;
+    
+    int area = area_superficie (l, w, h);
+    int volume = volume_total (l, w, h);
+    cout << "This solid has " << area << " units of area and " << volume << " units of volume.";
 
+}
--- My Drafts.cpp
+++ My Drafts.cpp
@@ -56,3 +56,34 @@ int main()
     double result = power(base, exponent);
     cout << "The result is " << result;
 }
+
+int area_superficie (int comprimento, int largura, int altura)
+{
+    int area; // Result
+    area = 2 * (comprimento * largura + comprimento * altura + largura * altura);
+    return area;
+}
+
+int volume_total (int comprimento, int largura, int altura)
+{
+    int volume; // Result
+    volume = comprimento * largura * altura;
+    return volume;
+}
+
+int main()
+{
+    int l, w, h;
+    cout << "Imagin that you have a solid. What big is it?" << endl;
+    cout << "Lenght: ";
+    cin >> l;
+    cout << "Width: ";
+    cin >> w;
+    cout << "Height: ";
+    cin >> h;
+    
+    int area = area_superficie (l, w, h);
+    int volume = volume_total (l, w, h);
+    cout << "This solid has " << area << " units of area and " << volume << " units of volume.";
+
+}
