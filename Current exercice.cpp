45322ccfbe365ff7492d30e734894e8c5941e330
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 16:10:00 +0000

    Basic strings statements

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -6,8 +6,12 @@ using namespace std;
 
 int main()
 {
-    string greeting = "Hello World!";
-    cout << greeting + " for everyone" << endl;
-    cout << greeting.length() << endl;
-    // 02:34:18 in video
+    string text;
+    getline(cin, text);
+    cout << text << endl; // Return the input
+    cout << text.length() << endl; // Return input's lenght
+    cout << text.erase(text.length() - 1); // Delete last letter
+    cout << text.pop_back(); // Delete last letter too
+    cout << text.insert(5, " something "); // Append "something" in 5th index
+    cout << text.erase(3); // Delete letter in 3rd index
 }
