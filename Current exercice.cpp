ae188a9299976386ae55de88ad917ecec510ed30
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 19:31:16 +0000

    Add factorial version with while-statement

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 
-int factorial (int number)
+int factorial_1 (int number)
 {
     int fact = 1;
     for ( number = number ; number > 1; number--)
@@ -15,10 +15,21 @@ int factorial (int number)
     return fact;
 }
 
+int factorial_2 (int number)
+{
+    int i = 1, fact = 1;
+    while ( i <= number)
+    {
+        fact = fact * i;
+        i++;
+    }
+    return fact;
+}
+
 int main()
 {
     int number;
     cout << "Give me a number: " << endl;
     cin >> number;
-    cout << "The factorial of the number " << number << " is " << factorial(number) << endl;
+    cout << "The factorial of the number " << number << " is " << factorial_2(number) << endl;
 }
--- My Drafts.cpp
+++ My Drafts.cpp
@@ -240,7 +240,7 @@ int main() // Loops: I --> Initialize variable ; C --> Condition; U --> Update v
         cout << x << endl; }
 }
 
-int factorial (int number)
+int factorial_1 (int number)
 {
     int fact = 1;
     for ( number = number ; number > 1; number--)
@@ -250,10 +250,21 @@ int factorial (int number)
     return fact;
 }
 
+int factorial_2 (int number)
+{
+    int i = 1, fact = 1;
+    while ( i <= number)
+    {
+        fact = fact * i;
+        i++;
+    }
+    return fact;
+}
+
 int main()
 {
     int number;
     cout << "Give me a number: " << endl;
     cin >> number;
-    cout << "The factorial of the number " << number << " is " << factorial(number) << endl;
+    cout << "The factorial of the number " << number << " is " << factorial_2(number) << endl;
 }
