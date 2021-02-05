d3e8cdf3b7918104d36ec50619efaefe5f8e8609
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 14:18:50 +0000

    More integer functions

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -147,6 +147,11 @@ int main()
     cout << remainder(x, z) << endl; // Remainder between float and (float or integer)
     cout << fmax(x, y) << endl; // Max value
     cout << fmin(x, z) << endl; // Min value
+    cout << ceil(z) << endl; // Ceil function (1.25 --> 2)
+    cout << floor(z) << endl; // Floor function (1.25 --> 1)
+    cout << round(z) << endl; // Round function (same as Python)
+    cout << abs(-10.25) << endl; // Absolute value (negative --> positive, positive --> positive)
 }
+
 
 
