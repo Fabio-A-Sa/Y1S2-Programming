07ac6cfafee6a36e7a85bdbd111f33ebc1f9f993
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 17:23:21 +0000

    Base converter

--- Current exercice.cpp
+++ Current exercice.cpp
@@ -4,20 +4,27 @@
 #include <string>
 using namespace std;
 
+void converter (int number, int base)
+{
+    if (base == 1){
+        cout << "Number " << number << " is " << dec << number << " in decimal." << endl; }
+
+    if (base == 2) {
+        cout << "Number " << number << " is " << oct << number << " in octal" << endl; }
+
+    if (base == 3) {
+        cout << "Number " << number << " is " << hex << number << " in hexadecimal." << endl; }
+
+    else {
+        cout << "Your choice is not valid! Try again!" << endl; }
+}
+
 int main()
 {
-    string text1, text2;
-    cout << "To compare strings:" << endl;
-    cin >> text1;
-    cin >> text2;
-
-    if (text1 == text2)
-    {
-         cout << "Equal!" << endl;
-    }
-    else
-    {
-        cout << "Different!" << endl;
-    }
-
+    int number, base;
+    cout << "Welcome to the numerical base converter! Input a decimal number: " << endl;
+    cin >> number;
+    cout << "Which base do you like to convert?\n1 --> Decimal\n2 --> Octal\n3 --> Hexadecimal" << endl;
+    cin >> base;
+    converter (number, base);
 }
