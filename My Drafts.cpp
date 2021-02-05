f83b30323c32a4aa16bead5ccb6110369aed9c31
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-05 11:58:39 +0000

    First function

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -1,12 +1,9 @@
-// Created on February, 2021
-// @author: Fábio Araújo de Sá
-
-// Exercices from course https://www.youtube.com/watch?v=GQp1zzTwrIg 10 hours! Begin: 4th February 2021
-
 #include <iostream>
 
-int main():
+using std::cout;
+
+int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+    int slices = 10;
+    cout << "You Have" << slices << "of pizza" << std::endl;
 }
