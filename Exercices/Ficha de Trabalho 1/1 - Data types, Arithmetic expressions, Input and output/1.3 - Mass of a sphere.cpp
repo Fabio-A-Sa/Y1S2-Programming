d0b25f18f0933215918efcb15bbb2f9cff47dba1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-23 14:15:26 +0000

    1.3

--- Exercices/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.3 - Mass of a sphere.cpp
+++ Exercices/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.3 - Mass of a sphere.cpp
@@ -1,2 +1,20 @@
 // Created on February, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int pi = 3.14;
+    double densidade, raio, massa;
+
+    cout << "Valor da desidade da esfera, em quilogramas por metro cubico (Kg/m^3): ";
+    cin >> densidade;
+    cout << "Valor do raio da esfera, em metros (m): ";
+    cin >> raio;
+
+    massa = 4 / 3 * (densidade*pi*raio*raio*raio);
+    cout << "A esfera considerada tem " << massa << " Kg de massa aproximadamente." << endl;
+    return 0;
+}
