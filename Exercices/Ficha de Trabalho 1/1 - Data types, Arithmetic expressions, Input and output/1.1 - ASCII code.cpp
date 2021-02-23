d211bf4974a0397d8b138dbff2aab8f693f7f31e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-23 14:07:46 +0000

    1.1

--- Exercices/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.1 - ASCII code.cpp
+++ Exercices/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.1 - ASCII code.cpp
@@ -1,2 +1,13 @@
 // Created on February, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+    cout << "Please enter a letter: ";
+    cin >> letter;
+    cout << "ASCII code of '" << letter << "' : " << (int) letter << endl;
+}
