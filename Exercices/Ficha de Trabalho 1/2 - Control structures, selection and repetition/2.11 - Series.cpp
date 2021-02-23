dfb0e83a2c217cb7dacb5943bcc4868eec36296b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-23 18:52:57 +0000

    2.11

--- Exercices/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.11 - Series.cpp
+++ Exercices/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.11 - Series.cpp
@@ -2,4 +2,82 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int factorial (int number) 
+{
+    if (number <= 1) { return 1 ; }
+    else { return number * factorial(number - 1) ; }
+}
+
+void serie_a () 
+{
+    int n;
+    double result = 0; 
+
+    cout << "Quantas parcelas devo somar? ";
+    cin >> n;
+
+    for (int i = 1; i <= n ; i++) {
+
+        double denominador = 2*i-1;
+        if ( i%2 == 1) {
+            // Somar
+            result = result + 4/denominador;
+        }
+        else {
+            // Subtrair
+            result = result - 4/denominador;
+        }
+    }
+    cout << "O valor de pi: " << result << endl;
+}
+
+void serie_b ()
+{
+    int n;
+    double result = 0; 
+
+    cout << "Quantas parcelas devo somar? ";
+    cin >> n;
+
+    for (int i = 1; i < n ; i++) {
+
+        double denominador = factorial(i-1);
+        result = result + 1/denominador;
+
+    }
+    cout << "O valor de e: " << result << endl;
+}
+
+void serie_c ()
+{
+    int n, x;
+    double result = 1; 
+
+    cout << "Quantas parcelas devo somar? ";
+    cin >> n;
+    cout << "Qual o valor de x? ";
+    cin >> x;
+
+    for (int i = 1; i < n ; i++) {
+
+        double denominador = factorial(i);
+        if ( i%2 == 1) {
+            result = result - pow(x, i)/denominador;
+        }
+        else {
+            result = result + pow(x, i)/denominador;
+        }
+    }
+    cout << "O valor de e elevado a menos x: " << result << endl;
+}
+
+int main () 
+{   
+    serie_a () ;
+    serie_b () ;
+    serie_c () ;
+    return 0;
+}
