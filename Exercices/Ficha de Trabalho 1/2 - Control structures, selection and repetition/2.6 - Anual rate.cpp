feae1b9e18724abada93d1f06e262937e59f0b8f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-23 17:52:03 +0000

    2.6

--- Exercices/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.6 - Anual rate.cpp
+++ Exercices/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.6 - Anual rate.cpp
@@ -3,3 +3,20 @@
 
 #include <iostream>
 using namespace std;
+
+int main ()
+{
+    double n, q, j, result;
+
+    cout << "Quantos anos? ";
+    cin >> n;
+    cout << "Quantia inicial: ";
+    cin >> q;
+    cout << "Taxa de juros anuais, em percentagem: ";
+    cin >> j;
+
+    result = q*pow((1+j), n);
+
+    cout << "No final de " << n << " anos deveria ter " << result << " euros no banco!" << endl;
+    return 0;
+}
