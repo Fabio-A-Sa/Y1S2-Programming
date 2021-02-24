f2e99ba25a415d5c1a80ca4961cd3a9034481a15
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-24 20:24:49 +0000

    Exercise 3.5

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.5 - Greatest common divisor.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.5 - Greatest common divisor.cpp
@@ -1,2 +1,43 @@
 // Created on February, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int greatest_common_divisor (int a, int b) {
+
+    if (a == b && a != 0 ) {
+        return a;
+    }
+    else if ( a*b == 0 ) {
+        int answer = a == 0 ? b : a;
+        return answer;
+    }
+    else {
+        while ( a != b ) {
+            if ( a > b ) {
+                a = a - b;
+            }
+            else if ( b > a ) {
+                b = b - a;
+            }
+            else {
+                return a;
+            }
+        }
+    }
+}
+
+int main ()
+{   
+    int input1, input2;
+    cout << "Enter a number: ";
+    cin >> input1;
+    cout << "Enter another number: ";
+    cin >> input2;
+
+    int answer = greatest_common_divisor(input1, input2);
+    cout << "The greatest common divisor of numbers " << input1 << " and " << input2 << " is " << answer << endl;
+    return 0;
+}
