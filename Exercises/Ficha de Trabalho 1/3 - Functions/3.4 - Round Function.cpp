71f51a3531f6460cea4b28f4c6162ea4d6cc0a4a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-24 19:29:20 +0000

    Exercise 3.4

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.4 - Round Function.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.4 - Round Function.cpp
@@ -1,2 +1,26 @@
 // Created on February, 2021
 // @author: Fábio Araújo de Sá
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double round (double x, unsigned n) 
+{   
+    double rounded = floor(x * (pow(10, n)) + 0.5) / pow(10, n) ;
+    return rounded;
+}
+
+int main () // Exercício 3.4 -> Round
+{   
+    double number;
+    int decimal_places;
+
+    cout << "Enter a floar number: ";
+    cin >> number;
+    cout << "Number of decimal places to round: ";
+    cin >> decimal_places;
+    cout << round (number, decimal_places);
+    
+    return 0;
+}
