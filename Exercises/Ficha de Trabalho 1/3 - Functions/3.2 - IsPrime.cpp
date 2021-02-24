a1c547940d40899bfc2180c52f408515843957f4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-24 19:26:36 +0000

    Exercise 3.2

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.2 - IsPrime.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.2 - IsPrime.cpp
@@ -1,2 +1,52 @@
 // Created on February, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+using namespace std;
+
+bool isPrime (int number) 
+{
+    bool flag = true;
+    for (int i = 2; i < number; i++) {
+        flag = flag && ( number % i != 0);
+    }
+    return flag;
+}
+
+void a ()
+{
+    int number;
+    cout << "Number? ";
+    cin >> number;
+
+    if (isPrime(number)) { cout << "This number is prime!" << endl ; }
+    else { cout << "This number isn't prime!" << endl; }
+}
+
+void b ()
+{
+    int number = 2, counter = 0;
+
+    do  {
+        if (isPrime(number)) { cout << number << " "; counter++; number++; }
+        else { counter++; number++; }
+        } while (counter < 1000);
+}
+
+void c ()
+{
+    int number = 2, counter = 0;
+
+    do  {
+        if (isPrime(number)) { cout << number << " "; counter++; number++; }
+        else { number++; }
+        } while (counter != 100);
+}
+
+int main () 
+{
+    a () ;
+    b () ;
+    c () ;
+    return 0;
+}
