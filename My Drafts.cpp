6c8d8ee42b96924ab2b2a6170577b92e4992d64c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-24 20:25:54 +0000

    Add more drafts!

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -2250,3 +2250,44 @@ void until_overflow_d ()
     }
     cout << cobaia << endl;
 } 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int greatest_common_divisor (int a, int b) {
+
+    if (a == b && a != 0 ) {
+        return a;
+    }
+    else if ( a*b == 0 ) {
+        int answer = a == 0 ? b : a;
+        return answer;
+    }
+    else {
+        while ( a != b ) {
+            if ( a > b ) {
+                a = a - b;
+            }
+            else if ( b > a ) {
+                b = b - a;
+            }
+            else {
+                return a;
+            }
+        }
+    }
+}
+
+int main ()
+{   
+    int input1, input2;
+    cout << "Enter a number: ";
+    cin >> input1;
+    cout << "Enter another number: ";
+    cin >> input2;
+
+    int answer = greatest_common_divisor(input1, input2);
+    cout << "The greatest common divisor of numbers " << input1 << " and " << input2 << " is " << answer << endl;
+    return 0;
+}
