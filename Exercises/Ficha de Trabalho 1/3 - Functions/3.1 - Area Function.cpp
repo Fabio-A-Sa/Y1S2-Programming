da5a8eb5fc17941b5b22ff2fc00b1d44fae5e401
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-24 19:21:23 +0000

    Add 3.1 resolution

---
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.1 - Area Function.cpp
@@ -0,0 +1,50 @@
+// Created on February, 2021
+// @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double distance (double x1, double x2, double y1, double y2) 
+{
+    double dist;
+    dist = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return dist;
+}
+
+double area (double p, double p1, double p2, double p3)
+{
+    double area;
+    area = sqrt(p*(p-p1)*(p-p2)*(p-p3));
+    return area;
+}
+
+int main()
+{
+    double ax, ay, bx, by, cx, cy;
+    double distance, area_total, l1, l2, l3, l_total;
+
+    // Input
+    cout << "Insira as coordenadas dos vertices do triangulo:" << endl;
+    cout << "Vertice A --> Abcissa (x):";
+    cin >> ax;
+    cout << "Vertice A --> Ordenada (y):";
+    cin >> ay;
+    cout << "Vertice B --> Abcissa (x):";
+    cin >> bx;
+    cout << "Vertice B --> Ordenada (y):";
+    cin >> by;
+    cout << "Vertice C --> Abcissa (x):";
+    cin >> cx;
+    cout << "Vertice C --> Ordenada (y):";
+    cin >> cy;
+
+    // Data
+    l1 = distance (ax, bx, ay, by);
+    l2 = distance (ax, cx, ay, cy);
+    l3 = distance (bx, cx, by, cy);
+    l_total = l1 + l2 + l3;
+    area_total = area (l_total, l1, l2, l3); 
+    cout << "O triangulo considerado tem " << area_total << " unidades de area." << endl;
+    return 0;
+}
