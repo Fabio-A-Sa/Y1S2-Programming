7c02c88d4a95b28021dc76bde6bad3d313197c43
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-28 18:09:21 +0000

    Add notes for lectures

---
+++ Notes/Theoretical lecture 1.cpp
@@ -0,0 +1,12 @@
+// Created on February 21, 2021
+// @author: Fábio Araújo de Sá
+// Theoretical lecture 1, page 1-
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{   
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ Notes/Theoretical lecture 2.cpp
@@ -0,0 +1,3 @@
+// Created on February 25, 2021
+// @author: Fábio Araújo de Sá
+// Theoretical lecture 1
