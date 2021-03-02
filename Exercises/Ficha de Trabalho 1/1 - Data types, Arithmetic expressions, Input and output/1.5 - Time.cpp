2ef16020180994849c14d6e25dca18a256305bcd
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-02 17:57:24 +0000

    Changed exercise

--- Exercises/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.5 - Time.cpp
+++ Exercises/Ficha de Trabalho 1/1 - Data types, Arithmetic expressions, Input and output/1.5 - Time.cpp
@@ -2,13 +2,14 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
+void exercise_a ()
 {
     int d, h1, h2, m1, m2, s1, s2, total, resto;
 
-    // Inputs and Strings --> Integers
+    // Inputs
     cout << "Time 1 (hours minutes seconds) ? ";
     cin >> h1 >> m1 >> s1;
     cout << "Time 2 (hours minutes seconds) ? ";
@@ -26,6 +27,41 @@ int main()
 
     // Output
     cout << "Time1 + Time2 = " << d << " days, " << h1 << " hours, " << m1 << " minutes and " << s1 << " seconds" << endl;
+}
 
+void exercise_b ()
+{
+    int d, h1, h2, m1, m2, s1, s2, total, resto;
+    string t1, t2;
+
+    // Inputs and Strings --> Integers
+    cout << "Time 1 (hours:minutes:seconds) ? ";
+    getline (cin, t1);
+    h1 = stoi(t1.substr (0,2));
+    m1 = stoi(t1.substr (3,2));
+    s1 = stoi(t1.substr (6,2));
+    cout << "Time 2 (hours:minutes:seconds) ? ";
+    getline (cin, t2);
+    h2 = stoi(t2.substr (0,2));
+    m2 = stoi(t2.substr (3,2));
+    s2 = stoi(t2.substr (6,2));
+
+    // Total of seconds, remainder...
+    total = (h1+h2)*(60*60) + (m1+m2)*(60) + (s1+s2);
+    d = total / (24*60*60);
+    total = total % (24*60*60);
+    h1 = total / (60*60);
+    total = total % (60*60);
+    m1 = total / 60;
+    s1 = total % 60;
+    
+    // Output
+    cout << "Time1 + Time2 = " << d << " days, " << h1 << " hours, " << m1 << " minutes and " << s1 << " seconds" << endl;
+}
+
+int main ()
+{
+    exercise_a () ;
+    exercise_b () ;
     return 0;
 }
