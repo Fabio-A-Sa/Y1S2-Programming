fb42a53035dd31851ceeefb3b96ba299d4d8e001
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-03 18:38:52 +0000

    More exercises

--- Exercises/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.14 - SquareRoot by Heron.cpp
+++ Exercises/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.14 - SquareRoot by Heron.cpp
@@ -2,19 +2,22 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
 using namespace std;
 
-void rounded (double number, double exponent)
+double rounded (double number, double exponent)
 {
     double n;
     n = floor (number * pow(10, exponent) + 0.5) / pow(10, exponent);
-    cout << n;
+    return n;
 }
 
 void srqt_heron_a ()
 {
-    double n, rq, rqn, delta, dif;
-    int nMaxIter, exponent = 0;
+    double n, rq, rqn, delta, dif = 50;
+    int nMaxIter, exponent = -6;
 
     cout << "Enter a number: ";
     cin >> n;
@@ -23,22 +26,26 @@ void srqt_heron_a ()
     cout << "Max iteration: ";
     cin >> nMaxIter;
 
+    for (char number: to_string(1 / delta)) {
+        if (number == '0') {
+            exponent++;
+        }
+    }
+
     int counter = 0;
     rq = 1;
     do  {
         rqn = (rq + n / rq) / 2;
-        dif = n - pow(rqn, 2);
+        dif = n - rqn;
         rq = rqn;
-        counter++;
-        } while (abs(dif) > delta && counter != nMaxIter);
-
+        } while (abs(dif) > delta);
     cout << rq << endl;
 }
 
 void heron_with_precision ()
 {
     double n, rq, rqn, delta, dif;
-    int nMaxIter, exponent = -6;
+    int exponent = -6;
 
     cout << "Enter a number: ";
     cin >> n;
@@ -54,11 +61,11 @@ void heron_with_precision ()
     rq = 1;
     do  {
         rqn = (rq + n / rq) / 2;
-        dif = n - pow(rqn, 2);
+        dif = n - rqn;
         rq = rqn;
         } while (abs(dif) > delta);
 
-    rounded (rqn, exponent);
+    cout << rq << endl;
 }
 
 int main ()
