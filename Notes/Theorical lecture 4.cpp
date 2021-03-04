99541ab049dcc4745ae3a2a39aa298e436616891
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-04 10:10:01 +0000

    Lecture

--- Notes/Theorical lecture 4.cpp
+++ Notes/Theorical lecture 4.cpp
@@ -6,9 +6,10 @@
 #include <cmath>
 #include <iomanip>
 #include <climits>
+#include <cctype>
 using namespace std;
 
-
+// Manipulation digits with setw and setfill
 int digits_manipulation ()
 {
     int dd, mm, yyyy;
@@ -16,13 +17,40 @@ int digits_manipulation ()
 
     cout << "Date (dd:mm:yyyy): ";
     cin >> dd >> sep1 >> mm >> sep2 >> yyyy;
+    cout << "Date: " << setfill('0') << setw(2) << dd << sep1 << setw(2) << mm << sep2 << setw(4) << yyyy << endl;
 
-    cout << "Date: " << setw(2) << dd << sep1 << setw(2) << mm << sep2 << setw(4) << yyyy << endl;
+    if (sep1 != ':' || sep2 != ':' || cin.peek() != '\n') {
+        cout << "Invalid date" << endl;
+    }
 
     return 0;
 }
 
-int main () 
+// Upper and lower strings --> Char by char #include <cctype>
+int upper_and_lower_strings () 
 {
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+    int length = sentence.length(); 
+
+    for (int i = 0; i < length; i++) { 
+
+        int c = sentence[i]; 
+        if (islower(c))  
+            sentence[i] = toupper(c); 
+        else if (isupper(c))  
+            sentence[i] = tolower(c); 
+        else
+            continue;
+    }
+
+    cout << sentence << endl;
+    return 0;
+}
+
+int main ()
+{
+    upper_and_lower_strings();
     return 0;
 }
