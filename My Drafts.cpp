5422496b70c0687de119f3da10acb78ec8123e43
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 21:28:04 +0000

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -2363,3 +2363,22 @@ int main () {
 
     return 0;
 }
+
+bool readInt(string x)
+{
+    bool flag = true;
+    for (char c : x) {
+        flag = flag && ( c == '0' || c == '1' ||c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' );
+    }
+    return flag;
+}
+
+int main () {
+
+    string input, answer;
+    cout << "Enter a integer number: ";
+    getline(cin, input);
+    answer = readInt(input) ? "True" : "False";
+    cout << answer << endl;
+    return 0;
+}
