733b91458835b0e4701c83cafcc61c36fc2dbe58
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 21:24:20 +0000

    Exercise 3.7

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.7 - Bool integer.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.7 - Bool integer.cpp
@@ -2,6 +2,26 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <vector>
 using namespace std;
 
+bool readInt(string x)
+{
+    bool flag = true;
+    for (char c : x) {
+        flag = flag && ( c == '0' || c == '1' ||c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' );
+    }
+    return flag;
+}
+
+int main () {
+
+    string input, answer;
+    cout << "Enter a integer number: ";
+    getline(cin, input);
+    answer = readInt(input) ? "True" : "False";
+    cout << answer << endl;
+    return 0;
+}
