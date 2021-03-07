61a1332924f3df66465c1cfc1cc4039d011fa5ba
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 22:02:24 +0000

    Exercise 3.8 a

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.8 - Fractions.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.8 - Fractions.cpp
@@ -6,9 +6,22 @@
 #include <cmath>
 using namespace std;
 
+bool readFraction(int &numerator, char option, int &denominator) {
 
+    bool flag = true;
+    flag = flag && (option == '/' && denominator != 0);
+    return flag;
+}
 
 int main()
 {
+    int a, b;
+    char op;
+    string answer;
+
+    cout << "Enter a fraction a/b with integer numbers: ";
+    cin >> a >> op >> b;
+    answer = readFraction(a, op, b) ? "Valid function" : "Invalid function";
+    cout << answer << endl;
     return 0;
 }
