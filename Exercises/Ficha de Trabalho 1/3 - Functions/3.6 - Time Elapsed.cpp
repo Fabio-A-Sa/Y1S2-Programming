98c71d86b514e022d8bf7222a74a6135188f94bc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 20:57:10 +0000

    Exercise 3.6

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.6 - Time Elapsed.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.6 - Time Elapsed.cpp
@@ -2,15 +2,32 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <ctime>
 using namespace std;
 
-
-double timeElapsed() {
-    return 0;
+void timeElapsed(int time) {
+    cout << time << endl;
 }
 
 int main () {
+
+    int time_t = 0, current_ti, current_tf, diff;
+    srand(time(NULL));
+    string answer;
+
+    do {
+        current_tf = 0;
+        current_ti = 0;
+        current_ti = time(NULL);
+        cout << "Call function?\nY --> Yes\nN --> No : ";
+        cin >> answer;
+        if (answer == "Y") {
+            current_tf = time(NULL);
+            diff = current_tf -current_ti;
+            time_t = time_t + diff;
+            timeElapsed(time_t);
+        }
+    } while (answer != "N") ;
+
     return 0;
 }
