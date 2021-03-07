6569b12341eeb5854d142ae46af32d43c41317ec
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 20:04:17 +0000

    Exercise modification

--- Exercises/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.16 - Basic Calculator.cpp
+++ Exercises/Ficha de Trabalho 1/2 - Control structures, selection and repetition/2.16 - Basic Calculator.cpp
@@ -5,39 +5,49 @@
 #include <string>
 using namespace std;
 
-// Falta coisas!
-
 int main ()
 {
-    double a, b, resultado;
-    string conta;
+    double a, b, resultado, memory = 0;
+    string conta, key;
 
-    cout << "Insira 2 numeros na calculadora:" << endl;
-    cout << "Primeiro numero: ";
-    cin >> a;
-    cout << "Segundo numero: ";
-    cin >> b;
-    cout << "Que conta deseja efetuar?\n+ --> a+b\n- --> a-b\nx --> axb\n/ --> a/b\nSua escolha: ";
-    cin >> conta;
+    do  {
+        cout << "Number operator number: " << endl;
+        cin >> a >> conta >> b;
 
-    if (conta == "+") {
-        resultado = a+b ;
-        cout << "a+b = " << resultado << endl;
-    }
-    else if (conta == "-") {
-        resultado = a-b ;
-        cout << "a-b = " << resultado << endl;
-    }
-    else if (conta == "x") {
-        resultado = a*b ;
-        cout << "axb = " << resultado << endl;
-    }
-    else if (conta == "/") {
-        resultado = a/b ;
-        cout << "a/b = " << resultado << endl;
-    }
-    else {
-        cout << "Opcao inexistente na calculadora!" << endl;
-    }
+        if (conta == "+") {
+            resultado = a+b ;
+            cout << a << " " << conta << " " << b << " = " << resultado << endl;
+        }
+        else if (conta == "-") {
+            resultado = a-b ;
+            cout << a << " " << conta << " " << b << " = " << resultado << endl;
+        }
+        else if (conta == "x") {
+            resultado = a*b ;
+            cout << a << " " << conta << " " << b << " = " << resultado << endl;
+        }
+        else if (conta == "/" || conta == ":") {
+            resultado = a/b ;
+            cout << a << " " << conta << " " << b << " = " << resultado << endl;
+        }
+
+        cout << "\nQ   --> Quit\nC   --> Clean contents\nM   --> Save current value\nMC  --> Clean all items in memory\n"
+        << "M+  --> Add current value in memory\nM-  --> Subtract current value in memory\nMR  --> Show contents of memory" << endl;
+        cin >> key;
+
+        if (key == "Q") { cout << "Finish process" << endl; }
+        else if (key == "C") { cout << "\x1B[2J\x1B[H" << endl; }
+        else if (key == "MC") { memory = 0; }
+        else if (key == "M+") { memory = memory + resultado; }
+        else if (key == "M-") { memory = memory - resultado; }
+        else if (key == "MR") {
+            cout << "Result = " << resultado << endl;
+            cout << "Memory = " << memory << endl;
+            cout << "Value A = " << a << endl;
+            cout << "Value B = " << b << endl;
+        }
+
+        } while (key != "Q");
+
     return 0;
 }
