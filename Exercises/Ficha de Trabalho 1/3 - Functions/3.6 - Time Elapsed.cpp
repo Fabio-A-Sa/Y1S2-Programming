b3b760c7abac57ab2fb42fb81cd434581840f5f0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 20:27:02 +0000

    Header

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.6 - Time Elapsed.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.6 - Time Elapsed.cpp
@@ -1,2 +1,16 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+double timeElapsed() {
+    return 0;
+}
+
+int main () {
+    return 0;
+}
