1e984f7b077e0e1c69a13d0c126567a74b03d772
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-07 21:01:37 +0000

    Update my drafts

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.7 - Bool integer.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.7 - Bool integer.cpp
@@ -1,2 +1,7 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
--- My Drafts.cpp
+++ My Drafts.cpp
@@ -2336,3 +2336,30 @@ int main()
     }
     return 0;
 }
+
+void timeElapsed(int time) {
+    cout << time << endl;
+}
+
+int main () {
+
+    int time_t = 0, current_ti, current_tf, diff;
+    srand(time(NULL));
+    string answer;
+
+    do {
+        current_tf = 0;
+        current_ti = 0;
+        current_ti = time(NULL);
+        cout << "Call function?\nY --> Yes\nN --> No : ";
+        cin >> answer;
+        if (answer == "Y") {
+            current_tf = time(NULL);
+            diff = current_tf -current_ti;
+            time_t = time_t + diff;
+            timeElapsed(time_t);
+        }
+    } while (answer != "N") ;
+
+    return 0;
+}
