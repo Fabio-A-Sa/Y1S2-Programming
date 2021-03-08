e9e8bfb1a3753c8e480809007451314de2caa607
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 21:31:50 +0000

    Exercise 3.11

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.11 - Euclid Recursive.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.11 - Euclid Recursive.cpp
@@ -1,2 +1,25 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+using namespace std;
+
+int gcd (int m, int n) {
+    
+    if (m % n == 0) {
+        return n;
+    }
+    else {
+        int remainder = m % n;
+        return gcd (n, remainder);
+    }
+}
+
+int main ()
+{
+    int a, b;
+    cout << "Please enter two integer numbers: ";
+    cin >> a >> b;
+    cout << "Greatest common divisor of numbers " << a << " and " << b << " is " << gcd(a, b) << endl;
+    return 0;
+}
