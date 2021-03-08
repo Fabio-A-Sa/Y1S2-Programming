be18e7e186096316fce4dc4f5c557222ab2a5a91
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 17:25:47 +0000

    Esboço

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.9 - Calendar.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.9 - Calendar.cpp
@@ -1,2 +1,13 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
