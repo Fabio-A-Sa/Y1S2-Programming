e9c79708bf8270f449984c0b79b22880d40bef34
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 21:41:06 +0000

    Added drafts

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -2937,3 +2937,23 @@ int main ()
     cout << "Max n for ULLONG integer is " << n-1 << endl;
     return 0;
 }
+
+int gcd (int m, int n) {
+    
+    if (m % n == 0) {
+        return n;
+    }
+    else {
+        int remainder = m % n;
+        return gcd (n, remainder);
+    }
+}
+
+int main ()
+{
+    int a, b;
+    cout << "Please enter two integer numbers: ";
+    cin >> a >> b;
+    cout << "Greatest common divisor of numbers " << a << " and " << b << " is " << gcd(a, b) << endl;
+    return 0;
+}
