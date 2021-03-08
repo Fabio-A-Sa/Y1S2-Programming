272caa1d8ab86886954d1dc97063ee2017b2772f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 18:16:47 +0000

    More

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.9 - Calendar.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.9 - Calendar.cpp
@@ -7,16 +7,41 @@
 #include <string>
 using namespace std;
 
-
-bool isBissexto (int year) {
+bool isLeapYear (int year) {
 
     bool flag = true;
-    
+    flag = flag && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ;
     return flag;
+}
+
+int numberDays (int month, int year) {
+    
+    int days;
+
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        days = 31;
+        break;
+    
+        case 4: case 6: case 9: case 11:
+        days = 30;
+        break;
+
+        case 2:
+            if (isLeapYear(year)) {
+                days = 29;
+            }
+            else {
+                days = 28;
+            }
+            break;
+    }
+
+    return days;
 }
 
 int main ()
 {
-    return 0;
+    cout << numberDays (3, 2016) << endl;
 }
 
