c7229fe7b8792fe06feca48867c6817d9559dbcf
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 22:31:47 +0000

    Exercise 3.14

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.14 - Integral equation.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.14 - Integral equation.cpp
@@ -3,11 +3,30 @@
 
 #include <iostream>
 #include <cmath>
-#include <string>
 using namespace std;
 
+double g (double x) {
+	return pow (x, 2);
+}
 
-int main ()
-{
-    return 0;
+double h (double x) {
+	return sqrt(4-pow(x, 2));
+}
+
+double integrateTR(double f(double), double a, double b, int n) {
+
+	double h = (b - a) / n;
+	double sum = 0;
+
+	for (int i = 1; i <= n; i++) {
+		sum += h / 2 * (f(a + (i - 1)*h) + f(a + i*h));
+	}
+	return sum;
+}
+
+int main() {
+
+	cout << integrateTR(g, 0, 10, 100) << endl;
+	cout << integrateTR(h, -2, 2, 100) << endl;
+	return 0;
 }
