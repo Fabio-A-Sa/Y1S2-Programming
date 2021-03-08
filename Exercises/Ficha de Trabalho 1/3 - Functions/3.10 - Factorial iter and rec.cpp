069aa7837adb0d592c43bf51960f41709681beb3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 21:07:45 +0000

    Factorial added

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.10 - Factorial iter and rec.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.10 - Factorial iter and rec.cpp
@@ -6,9 +6,32 @@
 #include <cmath>
 using namespace std;
 
+int factorial_ite(unsigned int n) {
 
+    unsigned long long total = 1;
+    while (n > 1) {
+        total = total * n;
+        n = n - 1;
+    }
+    return total;
+}
+
+int factorial_rec(unsigned int n) {
+
+    if (n == 1) {
+        return n;
+    }
+    else {
+        return n * factorial_rec(n-1)
+    }
+}
 
 int main ()
-{
+{   
+    unsigned int number; 
+
+    cout << "Number: ";
+    cin >> number;
+    cout << "The factorial of number " << number << " is " << factorial_ite(number) << endl;
     return 0;
 }
