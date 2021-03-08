c611b6f69ff3beb17b09cd35eb6985ee4fb3b4ba
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 22:16:00 +0000

    Exercise 3.13

--- Exercises/Ficha de Trabalho 1/3 - Functions/3.13 - Low and high.cpp
+++ Exercises/Ficha de Trabalho 1/3 - Functions/3.13 - Low and high.cpp
@@ -1,2 +1,25 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <cmath>
+#include <assert.h>
+using namespace std;
+
+int rollDie(int low = 1, int high = 6)
+{
+    assert(high >= low);
+    return (rand() % (high - low + 1)) + low;
+}
+
+int main ()
+{
+    rollDie();
+    return 0;
+}
+
+/* 
+A função retorna um valor aleatório entre 1 e 6. 
+A função assert verifica se a expressão "high >= low" é verdadeira.
+Caso seja, então o programa continua normalmente. Caso contrário, então para imediatamente a execução das tarefas.
+*/
