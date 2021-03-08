916b7f62a7a0b70f617d71e7a9502628a4fa30b6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-08 22:32:18 +0000

    Added drafts

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -3052,3 +3052,29 @@ int main ()
     rollDie();
     return 0;
 }
+
+double g (double x) {
+	return pow (x, 2);
+}
+
+double h (double x) {
+	return sqrt(4-pow(x, 2));
+}
+
+double integrateTR(double f(double), double a, double b, int n) {
+
+	double h = (b - a) / n;
+	double sum = 0;
+
+	for (int i = 1; i <= n; i++) {
+		sum += h / 2 * (f(a + (i - 1)*h) + f(a + i*h));
+	}
+	return sum;
+}
+
+int main() {
+
+	cout << integrateTR(g, 0, 10, 100) << endl;
+	cout << integrateTR(h, -2, 2, 100) << endl;
+	return 0;
+}
