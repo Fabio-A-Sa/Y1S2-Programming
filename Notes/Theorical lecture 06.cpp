f7d064c15f0be2a1e7595b364ca5b37ad2162743
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-11 09:18:01 +0000

    Main

--- Notes/Theorical lecture 06.cpp
+++ Notes/Theorical lecture 06.cpp
@@ -2,7 +2,23 @@
 // @author: Fábio Araújo de Sá
 // Theorical lecture 6, pages 45 --> ??
 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+const double PI = 3.1415;
+
+void swap (int a , int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main ()
 {
+    int x = 1, y = 2;
+    swap (x, y);
+    cout << 
     return 0;
 }
