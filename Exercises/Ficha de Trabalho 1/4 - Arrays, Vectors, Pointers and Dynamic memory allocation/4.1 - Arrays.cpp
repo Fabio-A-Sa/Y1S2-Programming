1fced463d2d03a655d153e600c4491f480574a10
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-16 15:48:07 +0000

    4.1

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
@@ -1,3 +1,15 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+#include <array>
+using namespace std;
+
+void  readArray(int  a[], size_t nElem) {
+
+}
+
+int main () 
+{
+    return 0;
+}
