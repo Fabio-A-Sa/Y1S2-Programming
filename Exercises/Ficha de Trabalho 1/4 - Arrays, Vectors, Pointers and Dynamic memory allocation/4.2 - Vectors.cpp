2920989cc5a1085353b7302a518036ce3aff1a00
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-16 17:45:22 +0000

    Modify

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.2 - Vectors.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.2 - Vectors.cpp
@@ -5,7 +5,95 @@
 #include <vector>
 using namespace std;
 
-int main ()
-{
+void readArray(vector<int> &v, size_t nElem) {
+
+    for (int index = 0 ; index < nElem ; index++ ) {
+        cout << index << ": " << v[index] << endl;
+    }
+
+}
+
+int findValueInArray(vector<int> &v, size_t nElem, int value, size_t pos1, size_t pos2) {
+
+    int answer = -1, loop = 0;
+    for (int index = pos1 ; index < pos2; index ++ ) {
+
+        loop ++;
+        if (v[index] == value) {
+            answer = index;
+            break;
+        }
+        if (loop == nElem) {
+            break;
+        }
+    }
+    return answer;
+}
+
+size_t findMultValuesInArray(const int a[], size_t nElem, int value, size_t pos1, size_t pos2, size_t index[]) {
+
+    size_t pointer = 0, loop = 0;
+    for (int i = pos1 ; i < pos2 ; i++ ) {
+
+        loop ++;
+        if (a[i] == value) {
+            index[pointer] = i;
+            pointer ++;
+        }
+        if (loop == nElem) {
+            break;
+        }
+    }
+    return pointer;
+}
+
+int main () 
+{   
+    int next_number;
+    vector<int> numbers;
+
+    while (!cin.eof()) {
+        cout << "Enter a number (letter for skip): ";
+        cin >> next_number;
+        numbers.push_back(next_number);
+    }
+    cin.clear();
+    cin.ignore('\n', 1000);
+    int size_of_vector = numbers.size();
+
+    // Read numbers, a)
+    do  {
+        cout << "How many numbers would you like to read? Integer number: ";
+        cin >> next_number;
+        if (next_number >= size_of_vector) {
+            cout << "Input error. Please try again with an integer number less than 15" << endl;
+        }
+        } while (next_number >= size_of_vector);
+    cout << "Value of " << next_number << "th number is ";
+    readArray(numbers, next_number);
+
+    // Search value, b)
+    int max_index, min_index, value, limit;
+    do  {
+        cout << "Min and max index to search (integer numbers between 0 and 15): ";
+        cin >> min_index >> max_index;
+        cout << "Which value would you like to search?";
+        cin >> value;
+        cout << "Max numbers to search: ";
+        cin >> limit;
+        if (min_index < 0 || max_index < 0 || max_index <= min_index || limit > size_of_array) {
+            cout << "Input error. Please try again." << endl;
+        }
+        } while (min_index < 0 || max_index < 0 || max_index <= min_index || limit > size_of_array);
+    int answer = findValueInArray(numbers, limit, value, min_index, max_index);
+    if (answer == -1) {
+        cout << "Not found!" << endl;
+    }
+    else {
+        cout << "Number " << value << " can be found in " << answer << "th position in array" << endl;
+    }
+
+    // Repeted numbers, e)
+    cout << "Your number has found " << findMultValuesInArray(numbers, limit, value, min_index, max_index, index) << " times in array" << endl;
     return 0;
 }
