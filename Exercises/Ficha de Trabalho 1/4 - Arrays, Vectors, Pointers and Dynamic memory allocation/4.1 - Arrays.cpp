37d9a82e0de5e8f298fa5fb0c796388ded32e682
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-16 15:55:30 +0000

    4.1.a

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
@@ -5,11 +5,17 @@
 #include <array>
 using namespace std;
 
-void  readArray(int  a[], size_t nElem) {
+void readArray(int a[], size_t nElem) {
+
+    for (int index = 0 ; index < nElem ; index++ ) {
+        cout << index << ": " << a[index] << endl;
+    }
 
 }
 
 int main () 
 {
+    int numbers[7] = {89, 56, 54, 1, 0, 89, 56} ;
+    readArray(numbers, 4);
     return 0;
 }
