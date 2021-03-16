2811a8e4f3e575726d1b819c3b89596df0dd131e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-16 16:47:56 +0000

    4.1.f

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.1 - Arrays.cpp
@@ -27,24 +27,24 @@ int findValueInArray(const  int  a[], size_t nElem, int value, size_t pos1, size
 
 size_t findMultValuesInArray(const int a[], size_t nElem, int value, size_t pos1, size_t pos2, size_t index[]) {
 
-    int pointer = 0;
+    size_t pointer = 0;
     for (int i = pos1 ; i < pos2 ; i++ ) {
         if (a[i] == value) {
             index[pointer] = i;
             pointer ++;
         }
     }
-    return index;
+    return pointer;
 }
 
 int main () 
 {
     const int size_of_array = 15;
-    int numbers[size_of_array] = {89, 56, 54, 1, 0, 89, 50, 58, 6, 8, 27, 158, 4, 33, 11} ;
-    size_t index[size_of_array] = {} ;
+    int numbers[size_of_array] = {89, 56, 54, 1, 0, 89, 50, 58, 6, 8, 27, 158, 89, 33, 11} ;
+    size_t index[size_of_array];
 
     readArray(numbers, 4);
     cout << findValueInArray(numbers, 15, 11, 3, 8) << endl;
-    cout << findMultValuesInArray(numbers, ) << endl;
+    cout << findMultValuesInArray(numbers, 15, 89, 1, 13, index) << endl;
     return 0;
 }
