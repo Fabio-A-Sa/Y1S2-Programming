eb253e0bd12fa7a62960ce3adfa0a849ab8e8680
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-17 01:49:21 +0000

    Mystery Gerês

--- pnpg.py
+++ pnpg.py
@@ -1,5 +1,5 @@
 
-def decrypt (message):
+def decrypt1 (message):
 
     from string import ascii_lowercase as abc
     for caracter in message:
@@ -21,8 +21,30 @@ def decrypt (message):
 
     print("Total = {}\nPrimeira parte = {}\nSegunda Parte = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
 
+def decrypt2 (message):
+
+    from string import ascii_lowercase as abc
+    for caracter in message:
+        if caracter in abc:
+            message = message.replace(caracter, str((abc.find(caracter)+1)%10))
+        else:
+            continue
+    
+    total_1 = 0
+    total_2 = 0
+    for par in message.split(" "):
+        try:
+            print(float(par))
+            total_2 = total_2 + float(par)
+        except ValueError:
+            total_1 = total_2
+            total_2 = 0
+            continue
+
+    print("Total = {}\nPrimeira parte = {}\nSegunda Parte = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
 
 code = "bch.ga dc.ag bia.jeh bf.jjj hc.gda bf.jjj ea.ahi bc.abb b.aai bbe.ibi gj.hcd cg.jib bcg.jae he.jcb + eh.igh fi.jdd"
-print(decrypt(code))
+print(decrypt1(code))
+print(decrypt2(code))
  
 
