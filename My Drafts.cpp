014ba7e874fd2d9326aa688b9434a4d6d832c8a9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 10:48:44 +0000

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -3536,3 +3536,46 @@ int main ()
     cout << "Your number has found " << findMultValuesInArray(numbers, limit, value, min_index, max_index, index) << " times in array" << endl;
     return 0;
 }
+
+void arrays ()  {
+
+    const int size_of_array = 10;
+    int numbers[size_of_array];
+
+    // Write numbers in array, number = index + 10
+    for (int index = 0 ; index < (sizeof(numbers) / sizeof(numbers[0])) ; index ++) {
+        numbers[index] = index + 10;
+    }
+
+    // Show values
+    for (int number: numbers) {
+        cout << "Number: " << number << endl;
+    }
+
+    // Show memory address
+    for (int index ; index < size_of_array ; index ++ ) {
+        cout << "Address of number " << numbers[index] << " is " << &numbers[index] << endl;
+    }
+}
+
+void multidimensional_arrays () {
+
+    const int size_of_array = 3;
+    // Matrix 3*3
+    int numbers[size_of_array][size_of_array];
+
+    // Input numbers to matrix
+    for (int main = 0; main < size_of_array ; main ++ ) {
+        for (int index = 0; index < size_of_array ; index ++ ) {
+            numbers[main][index] = index + 20;
+            cout << "Number " << main << "x" << index << " is " << numbers[main][index] << endl;
+        }
+    }
+}
+
+int main ()
+{  
+    arrays () ;
+    multidimensional_arrays () ;
+    return 0;
+}
