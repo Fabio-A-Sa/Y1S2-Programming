bfc03a843e410ad5e8a155c3af4437d2366766a0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 09:53:12 +0000

    Arrays

--- Notes/Theorical lecture 08.cpp
+++ Notes/Theorical lecture 08.cpp
@@ -3,10 +3,18 @@
 // Theorical lecture 8, pages 70 --> ??
 
 #include <iostream>
-#include <cmath>
+#include <array>
 using namespace std;
 
 int main () 
 {
+    const int size_of_array = 10;
+    int numbers[size_of_array];
+
+    for (int index = 0 ; index < (sizeof(numbers) / sizeof(numbers[0])) ; index ++) {
+        numbers[index] = index + 1;
+    }
+
     return 0;
 }
+
