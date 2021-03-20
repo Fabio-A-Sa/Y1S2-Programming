ee467a8db0bb6420a6542910516637671d7d4da2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-20 18:11:00 +0000

    more

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.5 - Remove duplicates.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.5 - Remove duplicates.cpp
@@ -6,10 +6,18 @@
 using namespace std;
 
 void removeDuplicates(vector<int> &v) {
-    
+
+
 }
 
 int main ()
-{
+{   
+    vector<int> example = {0, 9, 0, -5, 2, 9, 8, 0, 0, 8, 5, 2, 4, 15, 2} ;
+    removeDuplicates(example);
+
+    for (int number: example) {
+        cout << number << " ";
+    }
+
     return 0;
 }
