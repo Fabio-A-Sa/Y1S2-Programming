cbabd0712ded4298d1e4753217ab529c2045322b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-20 13:27:57 +0000

    4.4

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.4 - Binary Search.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.4 - Binary Search.cpp
@@ -8,12 +8,28 @@ using namespace std;
 
 int binarySearch(const vector<int> &v, int value) {
     
-    
+    int first = 0;
+    int last = v.size() - 1;
+    bool found = false;
 
+    while ( !found && first < last ) {
 
+        int middle = (first + last) / 2;
 
+        if (v[middle] == value) {
+            found = found || true;
+        }
+        else if (value < v[middle]) {
+            last = middle - 1;
+        }
+        else {
+            first = middle + 1;
+        }
+    }
 
-
+    if (found) 
+        return first;
+    return -1;
 }
 
 int main ()
@@ -22,10 +38,15 @@ int main ()
     srand(time(NULL));
 
     // Input numbers
-    for (int number = -5 ; number < 100 ; number = rand() % 6 + number + 1) {
+    for (int number = 0 ; number < 100 ; number = rand() % 6 + number + 1) {
         numbers.push_back(number);
     }
-    // example = {-5, -1, 3, 8, 9, 10, 12, 18, 24, 26, 29, 33, 37, 39, 40, 42, 48, 52, 58, 63, 68, 73, 74, 78, 80, 84, 88, 91, 93, 96}
+
+    for () {
+        
+    }
+    cout << "Numbers: "
+    // example = {0, 3, 8, 9, 10, 12, 18, 24, 26, 29, 33, 37, 39, 40, 42, 48, 52, 58, 63, 68, 73, 74, 78, 80, 84, 88, 91, 93, 96}
 
     return 0;
 }
