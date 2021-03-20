36f01fa2d24b060b4f11bdd6d0be2fcaed9a5486
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-20 18:17:03 +0000

    Update drafts

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -3734,3 +3734,35 @@ int main ()
     }
     return 0;
 }
+
+bool notInside(vector<int> &v, int value) {
+
+    bool flag = true;
+    for (int index = 0 ; index < v.size() ; index++ ) {
+        flag = flag && (v[index] != value) ;
+    }
+    return flag;
+}
+
+void removeDuplicates(vector<int> &v) {
+
+    vector<int> result;
+    for (int number: v) {
+        if (notInside(result, number)) {
+            result.push_back(number);
+        }
+    }
+    v = result;
+}
+
+int main ()
+{   
+    vector<int> example = {0, 9, 0, -5, 2, 9, 8, 0, 0, 8, 5, 2, 4, 15, 2} ;
+    removeDuplicates(example);
+
+    for (int number: example) {
+        cout << number << " ";
+    }
+
+    return 0;
+}
