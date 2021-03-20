f82de39ed4a51ca2867591308a3529d16e9437d0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-20 12:53:18 +0000

    Generate random numbers in vector

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.4 - Binary Search.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.4 - Binary Search.cpp
@@ -3,9 +3,19 @@
 
 #include <iostream>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 int main ()
-{
+{   
+    vector<int> numbers;
+    srand(time(NULL));
+
+    // Input numbers
+    for (int number = -5 ; number < 100 ; number = rand() % 6 + number + 1) {
+        numbers.push_back(number);
+    }
+    // example = {-5, -1, 3, 8, 9, 10, 12, 18, 24, 26, 29, 33, 37, 39, 40, 42, 48, 52, 58, 63, 68, 73, 74, 78, 80, 84, 88, 91, 93, 96}
+
     return 0;
 }
