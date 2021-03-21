d7e8c24d90376de3236571b77ede876032be9d3f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 12:44:58 +0000

    Add user inputs

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <array>
+#include <cmath>
+#include <vector>
 using namespace std;
 
 // Global constant
@@ -11,11 +13,11 @@ const int NE = 5;
 void localMax(const int a[][NE]) {
     
     cout << "Local maxima: " << endl;
-    for (int line = 1 ; line < NE ; line++ ) {
-        for (int row = 1 ; row < NE ; row++ ) {
-            if (a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] &&
-                a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row]    ) {
-                    cout << "Number " << a[line][row] << " in " << line << " line and " << row << " row of matrix" << endl;
+    for (int line = 1 ; line + 1 < NE ; line++ ) {
+        for (int row = 1 ; row + 1 < NE ; row++ ) {
+            if (a[line][row] > a[line+1][row] && a[line][row] > a[line-1][row] && a[line][row] > a[line][row+1] && a[line][row] > a[line][row-1] &&
+                a[line][row] > a[line+1][row+1] && a[line][row] > a[line+1][row-1] && a[line][row] > a[line-1][row+1] && a[line][row] > a[line-1][row-1]    ) {
+                    cout << "Number " << a[line][row] << " is in " << line << " line and " << row << " row of matrix" << endl;
                 }
             else {
                 continue;
@@ -24,15 +26,28 @@ void localMax(const int a[][NE]) {
     }
 }
 
-int main ()
-{   
+int main () 
+{       
+    // Example
     int matrix[][NE] = {{7, 3, 4, 1, 3}, 
                         {2, 9, 6, 2, 1}, 
                         {1, 3, 5, 1, 4}, 
                         {6, 5, 2, 7, 5}, 
                         {4, 2, 1, 3, 6}};
+    localMax(matrix);
 
+    // Input user values
+    int input;
+    int user_matrix[NE][NE];
+    for (int line = 0 ; line < NE ; line ++) {
+        for (int row = 0 ; row < NE ; row ++ ) {
+            cout << "Another number: ";
+            cin >> input;
+            user_matrix[line][row] = input;
+        }
+    }
+    localMax(user_matrix);
 
-
+    
     return 0;
 }
