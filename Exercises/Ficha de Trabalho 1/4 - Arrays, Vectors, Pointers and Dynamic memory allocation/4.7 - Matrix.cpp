c4e4810cf509f3fe16430192cf676cbdfa251a2d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 11:59:12 +0000

    Add a global constant

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
@@ -2,10 +2,21 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
-#include <vector>
+#include <array>
 using namespace std;
 
+// Global constant
+const int NE = 5;
+
+void localMax(const int a[][NE]) {
+
+}
+
 int main ()
-{
+{   
+    int matrix[][NE] = {{7, 3, 4, 1, 3}, {2, 9, 6, 2, 1}, {1, 3, 5, 1, 4}, {6, 5, 2, 7, 5}, {4, 2, 1, 3, 6}};
+
+
+
     return 0;
 }
