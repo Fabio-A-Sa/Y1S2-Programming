83b6c17d2875750c8ceca0de4665479706725a0e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 14:42:02 +0000

    Added main functions

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.8 - Days and years.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.8 - Days and years.cpp
@@ -4,9 +4,45 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 using namespace std;
 
+bool isLeapYear (int year) {
+
+    bool flag = true;
+    flag = flag && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ;
+    return flag;
+}
+
+int numberDays (int month, int year) {
+    
+    int days;
+
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        days = 31;
+        break;
+    
+        case 4: case 6: case 9: case 11:
+        days = 30;
+        break;
+
+        case 2:
+            if (isLeapYear(year)) {
+                days = 29;
+            }
+            else {
+                days = 28;
+            }
+            break;
+    }
+    return days;
+}
+
+
 int main ()
-{
+{   
+    int year;
+
     return 0;
 }
