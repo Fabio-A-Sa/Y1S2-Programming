2b6619f5b9102a864011ad539079b9529e2e8175
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 11:12:11 +0000

    Add primary functions

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.6 - Union and intersection.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.6 - Union and intersection.cpp
@@ -5,6 +5,75 @@
 #include <vector>
 using namespace std;
 
+void bubbleSort(vector<int> &v, char answer) {
+
+    int size_of_vector;
+    size_of_vector = v.size();
+
+    if (size_of_vector > 2) {
+
+        for (int index = 0 ; index < size_of_vector ; index ++) {
+
+            int aux;
+            int counter = 0;
+            bool flag = true;
+
+            while (counter + 1 < size_of_vector) {
+                
+                if (answer == 'a') {
+
+                    if (descending(v[counter], v[counter + 1])) {
+                        aux = v[counter+1];
+                        v[counter+1] = v[counter];
+                        v[counter] = aux;
+                        flag = flag && false;
+                    }
+                    counter++ ;
+
+                }
+
+                else if (answer == 'd') {
+
+                    if (ascending(v[counter], v[counter + 1])) {
+                        aux = v[counter+1];
+                        v[counter+1] = v[counter];
+                        v[counter] = aux;
+                        flag = flag && false;
+                    }
+                    counter++ ;
+
+                }
+            }
+
+            if (flag) {
+                break;
+            } 
+
+        }
+
+    }
+}
+
+bool notInside(vector<int> &v, int value) {
+
+    bool flag = true;
+    for (int index = 0 ; index < v.size() ; index++ ) {
+        flag = flag && (v[index] != value) ;
+    }
+    return flag;
+}
+
+void removeDuplicates(vector<int> &v) {
+
+    vector<int> result;
+    for (int number: v) {
+        if (notInside(result, number)) {
+            result.push_back(number);
+        }
+    }
+    v = result;
+}
+
 int main () 
 {
     return 0;
