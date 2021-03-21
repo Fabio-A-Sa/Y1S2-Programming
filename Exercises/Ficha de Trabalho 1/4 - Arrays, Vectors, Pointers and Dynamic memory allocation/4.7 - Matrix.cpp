bcb020d77e3597cbb033d296e8855393658443cc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 14:36:52 +0000

    4.7

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
@@ -120,14 +120,59 @@ void localMax_usingVector(vector<vector<int>> a, int nRows, int nLines, char ans
 }
 
 int main () 
-{       
-    vector<vector<int>> teste = {{7, 3, 4, 1, 3}, 
+{     
+
+    // Example
+    int matrix[][NE] = {{7, 3, 4, 1, 3}, 
                         {2, 9, 6, 2, 1}, 
                         {1, 3, 5, 1, 4}, 
                         {6, 5, 2, 7, 5}, 
                         {4, 2, 1, 3, 6}};
+                
+    localMax_usingArray(matrix);
 
-    localMax_usingVector(teste, 5, 5, 'Y');
+    // Input user values --> Array (2D)
+    int input;
+    int user_matrix[NE][NE];
+    for (int line = 0 ; line < NE ; line ++) {
+        for (int row = 0 ; row < NE ; row ++ ) {
+            cout << "Another number: ";
+            cin >> input;
+            user_matrix[line][row] = input;
+        }
+    }
+
+    localMax_usingArray(user_matrix);
+
+    // Input user values --> Vector (2D)
+    vector<vector<int>> matrix_vector;
+    vector<int> values;
+    int nRows, nLines, number;
+    cout << "Number of rows: ";
+    cin >> nRows;
+    cout << "Number of lines: ";
+    cin >> nLines;
+
+    for (int line = 0 ; line < nLines ; line ++ ) {
+        for (int row = 0 ; row < nRows ; row ++ ) {
+            cout << "Input a number: ";
+            cin >> number;
+            values.push_back(number);
+        }   
+        matrix_vector.push_back(values);
+        values.clear();
+    }
+
+    char answer;
+    do  {
+        cout << "Do you want to consider that any element of the matrix can be a local maximum? Y/N : ";
+        cin >> answer;
+        if (answer != 'Y' && answer != 'N') {
+            cout << "Invalid input. Please try again." << endl;
+        }
+        } while (answer != 'Y' && answer != 'N');
+
+    localMax_usingVector(matrix_vector, nRows, nLines, answer);
 
     return 0;   
 }
