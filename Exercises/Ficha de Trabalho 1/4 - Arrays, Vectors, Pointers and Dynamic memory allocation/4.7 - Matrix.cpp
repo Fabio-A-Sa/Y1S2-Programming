aab898c795ec260622f1f8f3ae0d75fa128e6f5d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-21 12:18:23 +0000

    Add conditions

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.7 - Matrix.cpp
@@ -9,12 +9,28 @@ using namespace std;
 const int NE = 5;
 
 void localMax(const int a[][NE]) {
-
+    
+    cout << "Local maxima: " << endl;
+    for (int line = 1 ; line < NE ; line++ ) {
+        for (int row = 1 ; row < NE ; row++ ) {
+            if (a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] &&
+                a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row] && a[line][row] > a[line][row]    ) {
+                    cout << "Number " << a[line][row] << " in " << line << " line and " << row << " row of matrix" << endl;
+                }
+            else {
+                continue;
+            }
+        }
+    }
 }
 
 int main ()
 {   
-    int matrix[][NE] = {{7, 3, 4, 1, 3}, {2, 9, 6, 2, 1}, {1, 3, 5, 1, 4}, {6, 5, 2, 7, 5}, {4, 2, 1, 3, 6}};
+    int matrix[][NE] = {{7, 3, 4, 1, 3}, 
+                        {2, 9, 6, 2, 1}, 
+                        {1, 3, 5, 1, 4}, 
+                        {6, 5, 2, 7, 5}, 
+                        {4, 2, 1, 3, 6}};
 
 
 
