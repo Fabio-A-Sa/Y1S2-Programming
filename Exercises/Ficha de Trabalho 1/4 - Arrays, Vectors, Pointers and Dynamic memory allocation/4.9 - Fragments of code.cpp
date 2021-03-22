e868f14636f19c0e95f67feb7870fc57547814e5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 13:17:02 +0000

    Begin

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.9 - Fragments of code.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.9 - Fragments of code.cpp
@@ -1,3 +1,18 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+using namespace std;
+
+void fragment_1 () {
+
+}
+
+void fragment_2 () {
+
+}
+
+int main ()
+{
+    return 0;
+}
