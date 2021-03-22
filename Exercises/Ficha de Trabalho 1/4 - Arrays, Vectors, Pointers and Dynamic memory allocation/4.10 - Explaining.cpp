c09a35005066e187117525dd78afbe5fe21f88e3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 13:45:02 +0000

    Add fragment

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.10 - Explaining.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.10 - Explaining.cpp
@@ -2,9 +2,24 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <array>
 using namespace std;
+
+void fragment () {
+
+    int values[] = { 2, 3, 5, 7, 11, 13 };
+    int *p = values+1;
+    cout << values[1] << endl;
+    cout << values+1 << endl;
+    cout << *p << endl;
+    cout << *(values+3) << endl;
+    cout << p+1 << endl;
+    cout << p[1] << endl;
+    cout << p-values << endl;
+}
 
 int main ()
 {
+    fragment();
     return 0;
 }
