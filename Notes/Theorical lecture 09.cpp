81ef7f80cb5103dde95689f776b072914b83c24f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 12:08:16 +0000

    More vector's statements

--- Notes/Theorical lecture 09.cpp
+++ Notes/Theorical lecture 09.cpp
@@ -12,6 +12,8 @@ int main ()
     vector<vector<int>> example = {{1, 2}, {2, 4}, {4, 7}};
     cout << example.at(2).at(1) << endl;
     cout << example[2][1] << endl;
-    cout << example.empty();                    // answer = exampleIsEmpty? 1 : 0;
+    cout << example.empty() << endl; // answer = exampleIsEmpty? 1 : 0;
+    example.clear();         // clean vector;
+    cout << example.empty() << endl; // answer = exampleIsEmpty? 1 : 0;
     return 0;
 }
