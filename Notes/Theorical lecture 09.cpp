5fe9128cac59a6852f3f781c2baf1434e30324db
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 11:02:03 +0000

    Begin

--- Notes/Theorical lecture 09.cpp
+++ Notes/Theorical lecture 09.cpp
@@ -2,3 +2,12 @@
 // @author: Fábio Araújo de Sá
 // Theorical lecture 8, pages 81 --> ??
 
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
