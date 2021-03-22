8e8a67c1854de43212b54b47c19ba2de608a4032
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 13:20:41 +0000

    Add grafments

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.9 - Fragments of code.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.9 - Fragments of code.cpp
@@ -6,13 +6,31 @@ using namespace std;
 
 void fragment_1 () {
 
+    int x = 1, y = 2;
+    int &ref_x = x, &ref_y = y;
+    ref_x = ref_y;
+    cout << "x = " << x << "; y = " << y << endl;
+    cout << "ref_x = " << ref_x << "; ref_y = " << ref_y << endl;
 }
 
 void fragment_2 () {
 
+    int x = 1, y = 2;
+    int *ptr_x = &x, *ptr_y = &y;
+    ptr_x = ptr_y;
+    cout << "x = " << x << "; y = " << y << endl;
+    cout << "ptr_x = " << ptr_x << "; ptr_y = " << ptr_y << endl;
+    cout << "*ptr_x = " << *ptr_x << "; *ptr_y = " << *ptr_y << endl;
 }
 
 int main ()
 {
+    fragment_1();
+
+    //
+
+    fragment_2();
+
     return 0;
 }
+
