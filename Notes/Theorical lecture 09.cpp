8f0cc73b4d9ec840543d1599825f264f10096863
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 12:06:38 +0000

    Add pages

--- Notes/Theorical lecture 09.cpp
+++ Notes/Theorical lecture 09.cpp
@@ -1,6 +1,6 @@
 // Created on March 22, 2021
 // @author: Fábio Araújo de Sá
-// Theorical lecture 8, pages 81 --> 95
+// Theorical lecture 8, pages 81 --> 102
 
 #include <iostream>
 #include <vector>
