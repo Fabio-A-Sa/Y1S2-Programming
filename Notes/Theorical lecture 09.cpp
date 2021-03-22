275e3e39c709498b39926803c29215b2c3471fff
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 11:50:37 +0000

    Using vectors

--- Notes/Theorical lecture 09.cpp
+++ Notes/Theorical lecture 09.cpp
@@ -3,11 +3,15 @@
 // Theorical lecture 8, pages 81 --> ??
 
 #include <iostream>
-#include <array>
 #include <vector>
 using namespace std;
 
+// Using vectors
 int main ()
 {
+    vector<vector<int>> example = {{1, 2}, {2, 4}, {4, 7}};
+    cout << example.at(2).at(1) << endl;
+    cout << example[2][1] << endl;
+    cout << example.empty();
     return 0;
 }
