1905a906e6c1a4d4c00f31eae7276f1e1017baee
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-22 13:53:52 +0000

    4.10

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.10 - Explaining.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.10 - Explaining.cpp
@@ -9,13 +9,13 @@ void fragment () {
 
     int values[] = { 2, 3, 5, 7, 11, 13 };
     int *p = values+1;
-    cout << values[1] << endl;
-    cout << values+1 << endl;
-    cout << *p << endl;
-    cout << *(values+3) << endl;
-    cout << p+1 << endl;
-    cout << p[1] << endl;
-    cout << p-values << endl;
+    cout << values[1] << endl;          // 3
+    cout << values+1 << endl;           // memory adress
+    cout << *p << endl;                 // pointer == 3
+    cout << *(values+3) << endl;        // values[4] == 7
+    cout << p+1 << endl;                // memory adress + 4 bits
+    cout << p[1] << endl;               // 5
+    cout << p-values << endl;           // 5 - 4 == 1
 }
 
 int main ()
