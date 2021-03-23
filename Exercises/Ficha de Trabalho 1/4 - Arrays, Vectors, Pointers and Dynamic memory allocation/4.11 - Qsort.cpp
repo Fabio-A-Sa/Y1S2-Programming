72c79a5a7da4ea82869ab8d1bc06c70a88b2aae9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-23 16:24:05 +0000

    More

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
@@ -10,16 +10,15 @@ int comparator (const void* number1 , const void* number2) {
     return (*(int*)number1 - *(int*)number2);
 }
 
-void qsort (void* base, size_t num, size_t size, int (*comparator)(const void*,const void*)) {
-
-    
-}
-
 int main ()
 {
     int number;
     
     int example[] = {120, 2, 5, -9, 8, 52, -1, 0, 6} ;
+
+    qsort (void* base, size_t num, size_t size, int (*comparator)(const void*,const void*))
+
+
     qsort(example);
     for (int number : example) {
         cout << number << " ";
