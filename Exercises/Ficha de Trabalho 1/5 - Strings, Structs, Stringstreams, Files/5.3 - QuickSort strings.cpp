036379cc965ba1fb0e81e9115a23d3640f036217
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-24 20:29:15 +0000

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
@@ -1,2 +1,13 @@
 // Created on March, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
