ddec5f77c27934beb67965576d0187ff7d370b5d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-24 20:29:57 +0000

    5.3

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
@@ -7,7 +7,23 @@
 #include <string>
 using namespace std;
 
-int main()
+// Não funciona --> Verificar mais tarde!
+
+int comparator ( const void *arg1, const void *arg2 ) {
+   return  *arg1 - *arg2 ;
+}
+
+int main( int argc, char **argv )
 {
-    return 0;
+    int arr[5] = {5, 8, 6, 4, 1} ;
+    int base = arr[0];
+    size_t size = 5;
+    size_t num;
+
+    qsort (void* base, size_t num, size_t size, int (*comparator)(const void*,const void*));    
+
+    // Print sorted data
+    for( i = 0 ; i < size ; ++i )
+        cout << arr[i] << " " ;
+
 }
