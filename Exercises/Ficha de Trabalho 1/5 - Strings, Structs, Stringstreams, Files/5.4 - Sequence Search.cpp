4b82a8fa5f22e0c58d8bc58693229f8f6f12dbce
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-24 21:02:08 +0000

    my own

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.4 - Sequence Search.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.4 - Sequence Search.cpp
@@ -5,7 +5,33 @@
 #include <string>
 using namespace std;
 
+bool my_own (const string &s, int nc, char c) {
+
+    int counter = 0;
+    for (char letter : s) {
+        if (letter == c) {
+            counter = counter + 1;
+            if (counter == nc) {
+                return true;
+            }
+        }
+        else {
+            counter = 0;
+        }
+    }
+    return false;
+}
+
+bool using_find () {
+
+
+}
+
 int main ()
 {
+    string sequence = "abcddeeddf" ;
+    int times = 3 ;
+    char letter = 'd' ;
+    cout << my_own (sequence, times, letter);
     return 0;
 }
