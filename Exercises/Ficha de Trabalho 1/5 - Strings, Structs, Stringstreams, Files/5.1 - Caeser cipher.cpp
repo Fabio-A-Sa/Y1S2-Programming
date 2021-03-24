afc8ad20c4f8f762992f11e5b328c304f37d2ec1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-24 19:17:01 +0000

    5.1a

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.1 - Caeser cipher.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.1 - Caeser cipher.cpp
@@ -6,7 +6,27 @@
 #include <string>
 using namespace std;
 
+// Global string
+string abc = "abcdefghijklmnopqrstuvwxyz";
+
+char encryptChar(char c, int key) {
+
+    int index;
+
+    if (isupper(c)) {
+        index = (abc.find(tolower(c)) + key) % 26;
+        return toupper(abc[index]);
+    }   
+    else {
+        index = (abc.find(c) + key) % 26;
+        return tolower(abc[index]);
+    }
+}
+
 int main ()
 {
+    int offset = 3;
+    char letter = 'a';
+    cout << encryptChar(letter, offset) << endl;
     return 0;
 }
