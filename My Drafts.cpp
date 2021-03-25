718f71d0a3d26265887a724aacc1ed394f94bfeb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-25 09:05:39 +0000

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -4381,3 +4381,22 @@ int hexToDec(std::string hexString)
     answer = (0x) hexString;
     return answer;
 }
+
+int comparator ( const void *arg1, const void *arg2 ) {
+   return  *arg1 - *arg2 ;
+}
+
+int main( int argc, char **argv )
+{
+    int arr[5] = {5, 8, 6, 4, 1} ;
+    int base = arr[0];
+    size_t size = 5;
+    size_t num;
+
+    qsort (void* base, size_t num, size_t size, int (*comparator)(const void*,const void*));    
+
+    // Print sorted data
+    for( i = 0 ; i < size ; ++i )
+        cout << arr[i] << " " ;
+
+}
