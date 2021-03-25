0d94b64ef7c1ff7ba9d8d0381108b51bd393dbe2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-25 09:07:37 +0000

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -4762,3 +4762,48 @@ int main( int argc, char **argv )
         cout << arr[i] << " " ;
 
 }
+
+bool my_own (const string &s, int nc, char c) {
+
+    int counter = 0;
+    for (char letter : s) {
+        if (letter == c) {
+            counter = counter + 1;
+            if (counter == nc) {
+                return true;
+            }
+        }
+        else {
+            counter = 0;
+        }
+    }
+    return false;
+}
+
+bool using_find (const string &s, int nc, char c) {
+
+    string search;
+    for (int number = 0 ; number < nc ; number ++ ) {
+        search = search + c ;
+    }
+    return s.find(search) < s.size();
+}
+
+int main ()
+{
+    string sequence, answer1, answer2 ;
+    int times ;
+    char letter ;
+
+    cout << "Sequence: ";
+    getline(cin, sequence);
+    cout << "Times: ";
+    cin >> times;
+    cout << "Letter: ";
+    cin >> letter;
+
+    answer1 = my_own (sequence, times, letter) ? "My own function: true" : "My own function: false" ;
+    answer2 = using_find (sequence, times, letter) ? "Using Find() method: true" : "Using Find() method: false";
+    cout << answer1 << endl << answer2 << endl;
+    return 0;
+}
