6009bbd05db154574dd5c899432a2c5c3c9aa0cc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-25 09:44:33 +0000

    Pointers with functions

--- Notes/Theorical lecture 10.cpp
+++ Notes/Theorical lecture 10.cpp
@@ -7,15 +7,36 @@
 #include <array>
 using namespace std;
 
+// Auxiliar functions
+
 void showArray (const int *a , int size) {
 
     for (int i = 0 ; i < size ; i++ ) {
         cout << "a[" << i << "] = " << *(a+i) << endl;
     }
 }
-int main ()
-{
+
+int sum (int x , int y) {
+    return x + y;
+}
+
+void pointers_numbers () {
+
     int example[] = {9, 10, 56, 8, 4} ;
     showArray(example, 5);
+}
+
+void pointers_functions () {
+    int result;
+    int (*p)(int, int);
+    p = sum;
+    result = (*p)(5, 8);
+    cout << "Sum: " << result << endl;
+}
+
+int main ()
+{
+    pointers_numbers();
+    pointers_functions();
     return 0;
 }
