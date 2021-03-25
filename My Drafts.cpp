d53bdc8ba0c9cffff968d066781a0918646e6f4e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-25 09:06:48 +0000

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -4568,3 +4568,84 @@ int main ()
 
     return 0;
 }
+
+// Global string
+string abc = "abcdefghijklmnopqrstuvwxyz";
+
+char encryptChar2(char c, int key) {
+    char solution = islower(c) ? abc[(abc.find(c) + key) % 26] : toupper(abc[(abc.find(tolower(c)) + key) % 26]) ;
+    return solution;
+}
+
+char encryptChar(char c, int key) {
+
+    int index;
+
+    if (isupper(c)) {
+        index = (abc.find(tolower(c)) + key) % 26;
+        return toupper(abc[index]);
+    }   
+    else if (islower(c)) {
+        index = (abc.find(c) + key) % 26;
+        return tolower(abc[index]);
+    }
+    else {
+        // Spaces, numbers and others characters
+        return c;
+    }
+}
+
+string encryptString(string s, int key) {
+
+    string solution;
+    for (char letter : s) {
+        solution = solution + encryptChar(letter, key);
+    }
+    return solution;
+}
+
+int main ()
+{
+    int offset;
+    string sentence;
+    char answer;
+
+    cout << "Your sentence: ";
+    getline(cin, sentence);
+
+    do  {
+        cout << "Key: ";
+        cin >> offset;
+        if (cin.fail()) {
+            cout << "Invalid key. Please enter a integer number. " << endl;
+        }
+        } while (!cin.fail());
+
+    do  {
+        cout << "Encryption or decryption? E/D : ";
+        cin >> answer;
+        if (cin.fail()) {
+            cout << "Invalid input. Please try again" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');  
+        }
+        if (tolower(answer) != 'e' && tolower(answer) != 'd') {
+            cout << "Invalid letter. Please choose E or D. " << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');  
+        } 
+
+        } while (!cin.fail() && (tolower(answer) != 'e' && tolower(answer) != 'd')) ;
+
+    if (tolower(answer) == 'e') {
+        cout << "Encryption: " << encryptString(sentence, offset) << endl;
+    }
+    else if (tolower(answer) == 'd') {
+        cout << "Decryption: " << encryptString(sentence, -offset) << endl;
+    }
+    else {
+        cout << "Invalid input. Please try again." << endl;
+    }
+
+    return 0;
+}
