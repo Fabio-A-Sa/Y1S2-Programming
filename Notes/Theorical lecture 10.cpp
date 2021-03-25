432dcdd33c652095e231e5977d8d034b5f478002
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-25 09:36:33 +0000

    Pointers

--- Notes/Theorical lecture 10.cpp
+++ Notes/Theorical lecture 10.cpp
@@ -7,7 +7,15 @@
 #include <array>
 using namespace std;
 
+void showArray (const int *a , int size) {
+
+    for (int i = 0 ; i < size ; i++ ) {
+        cout << "a[" << i << "] = " << *(a+i) << endl;
+    }
+}
 int main ()
 {
+    int example[] = {9, 10, 56, 8, 4} ;
+    showArray(example, 5);
     return 0;
 }
