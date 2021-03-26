23a6d41d97ad3836992d4b2248bb9c8ae5ab2c31
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-26 18:27:45 +0000

    Add modification

--- Tic Tac Toe/Main.cpp
+++ Tic Tac Toe/Main.cpp
@@ -3,13 +3,21 @@
 
 using namespace std;
 
-void printBoard (int &jogadas[]) {
+void printBoard (char jogadas[]) {
 
-    string line = "" ;
+    cout << "#############" << endl;
+    cout << "# " << jogadas[0] << " # " << jogadas[1] << " # "X #" << endl;
+    cout << "#############" << endl;
+    cout << "# X # O # X #" << endl;
+    cout << "#############" << endl;
+    cout << "# X # O # X #" << endl;
+    cout << "#############" << endl;
 }
 
 int main () 
 {
+    char jogadas[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'} ;
+    printBoard(jogadas);
     return 0;
 }
 
