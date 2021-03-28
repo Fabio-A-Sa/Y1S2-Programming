3e0cec2ba5b32a798cfc4981630c4f0846408346
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-28 18:11:54 +0100

    4.3 using C

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.3.1 - Bublesort in C .c
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.3.1 - Bublesort in C .c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
-void main () {
+int main () {
 
-    
+    int total = 15 ;
+    int array[] = {40, 55, 11, 32, 67, 5, 74, 89, 38, 66, 27, 36, 79, 99, 2} ;
+
+    int i, j, swap;
+
+    for ( i = 0 ; i < total - 1 ; i++ ) {   
+        for ( j = 0 ; j < total - i ; j++ ) {
+            if (array[j] > array[j+1]) {
+                swap = array[j+1];
+                array[j] = array[j+1];
+                array[j+1] = swap;
+            }
+        }
+    }
+
+    int index = 0;
+    printf("Ordenado: ");
+    while (index != total) {
+        printf("%d, ", array[index]);
+        index++;
+    }
 }
 
 
