613a81e1437c131785d27c031890ad0d98be83a5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-28 18:16:52 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -4969,6 +4969,24 @@ int main ()
     return 0;
 }
 
+void printBoard (char jogadas[]) {
+
+    cout << "#############" << endl;
+    cout << "# " << jogadas[0] << " # " << jogadas[1] << " # " << jogadas[2] << " #" << endl;
+    cout << "#############" << endl;
+    cout << "# " << jogadas[3] << " # " << jogadas[4] << " # " << jogadas[5] << " #" << endl;
+    cout << "#############" << endl;
+    cout << "# " << jogadas[6] << " # " << jogadas[7] << " # " << jogadas[8] << " #" << endl;
+    cout << "#############" << endl;
+}
+
+int main () 
+{
+    char jogadas[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'} ;
+    printBoard(jogadas);
+    return 0;
+}
+
 int main () {
 
     int total = 15 ;
