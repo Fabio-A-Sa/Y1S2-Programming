1ad9e1d862f1dac06b6bbc786aa9f1e76ea9ae86
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-29 11:08:08 +0100

    Begin

--- Notes/Theorical lecture 11.cpp
+++ Notes/Theorical lecture 11.cpp
@@ -1,3 +1,13 @@
 // Created on March 29, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 11, pages 107 --> ???
+
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+
+int main () 
+{
+    return 0;
+}
