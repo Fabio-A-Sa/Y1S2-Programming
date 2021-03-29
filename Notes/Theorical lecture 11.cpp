d048cdcc4cbd63fbe0fbcb6af1f6a8d83a4d9b21
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-29 11:26:20 +0100

    Add function

--- Notes/Theorical lecture 11.cpp
+++ Notes/Theorical lecture 11.cpp
@@ -7,7 +7,29 @@
 #include <array>
 using namespace std;
 
-int main () 
+#define NLIN 2
+#define NCOL 3
+
+void showArray (int a[][NCOL], int numLines, int numCols) {
+    for (int i = 0 ; i < numLines ; i++) {
+        for (int j = 0 ; j < numCols ; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void static_2d_array ()
 {
+    int a[NLIN][NCOL];
+    for (int i=0; i<NLIN; i++)
+        for (int j=0; j<NCOL; j++)
+            a[i][j] = 10*(i+1)+j;
+    showArray(a, NLIN, NCOL);
+}
+
+int main ()
+{   
+    static_2d_array();
     return 0;
 }
