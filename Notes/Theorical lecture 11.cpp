3394fbeeccc0ce322b1b4cf0b042dee9fa3ee052
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-29 11:51:11 +0100

    Dinamic allocation

--- Notes/Theorical lecture 11.cpp
+++ Notes/Theorical lecture 11.cpp
@@ -7,9 +7,6 @@
 #include <array>
 using namespace std;
 
-#define NLIN 2
-#define NCOL 3
-
 void show_static_Array (int a[][NCOL], int numLines, int numCols) {
     for (int i = 0 ; i < numLines ; i++) {
         for (int j = 0 ; j < numCols ; j++) {
@@ -21,6 +18,8 @@ void show_static_Array (int a[][NCOL], int numLines, int numCols) {
 
 void static_allocation_2d_array () {
     
+    unsigned const int NLIN = 2;
+    unsigned const int NCOL = 3;
     int a[NLIN][NCOL];
     for (int i=0; i<NLIN; i++)
         for (int j=0; j<NCOL; j++)
@@ -28,19 +27,34 @@ void static_allocation_2d_array () {
     show_static_Array(a, NLIN, NCOL);
 }
 
-void show_dinamic_array(int * a, int NLIN, int NCOL) {
+void show_dinamic_array(int **a, int NLIN, int NCOL) {
 
+    for (int i = 0 ; i < NLIN ; i++) {
+        for (int j = 0 ; j < NCOL ; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 void dinamic_allocation_2d_array () {
 
     int numLines = 3;
     int numCols = 4;
-    int * a = (int *) malloc(numLines * numCols * sizeof(int)) ;
+    int ** a = (int **) malloc(numLines * sizeof(int*)) ;
     for (int i = 0; i < numLines; i++)
-        for (int j=0; j<NCOL; j++)
+        a[i] = (int *) malloc(numCols * sizeof(int));
 
-    show_dinamic_array(*a, numLines, numCols);
+    for (int i = 0 ; i < numLines ; i++)
+        for (int j = 0 ; j < numCols ; j++ )
+            a[i][j] = 10 * (i + 1) + j ;
+
+    show_dinamic_array(a, numLines, numCols);
+
+    // Free memory
+    for (int i = 0 ; i < numLines ; i++ )
+        free(a[i]);
+    free(a);
 }
 
 int main ()
