dfe6ca366b3e6c7a077e5767b6ba103ba64d7f7d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 17:35:41 +0100

    Playing with FEUP's location

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.9 - Struct.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.9 - Struct.cpp
@@ -5,9 +5,17 @@
 #include <vector>
 using namespace std;
 
-
+struct Address {
+    string street;
+    int door_number;
+    string city;
+};
 
 int main ()
 {
+    Address FEUP;
+    FEUP.street = "Rua Dr Roberto Frias";
+    FEUP.door_number = 145;
+    FEUP.city = "Porto";
     return 0;
 }
