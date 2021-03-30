fa6c9f90d958b1adc378c1e5d17cc77f56269c57
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 12:23:29 +0100

    Using functions

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.7 - Date normalize.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.7 - Date normalize.cpp
@@ -3,14 +3,24 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 struct Date {
     unsigned int year, month, day;
-} Date;
+};
 
+void readDate (Date *d) {
+
+    char character;
+    cout << "Enter a date using format YYYY/MM/DD :";
+    cin >> d->year >> character >> d->month >> character >> d->day ;
+}
 
 int main () 
-{
+{   
+    srand(time(NULL));
+    Date *data;
+    readDate(data);
     return 0;
 }
