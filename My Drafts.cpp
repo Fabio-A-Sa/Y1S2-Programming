38183a2189d5cd711d36b0181ea6c86d34c3f071
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 17:13:38 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5125,3 +5125,37 @@ int main ()
     common_error();
     return 0;
 }
+
+struct Fraction {
+    int numerator;
+    int denominator;
+};
+
+bool readFraction(Fraction &fraction) {
+
+    if (fraction.denominator == 0)
+        return false;
+    return true;
+}
+
+int main () 
+{   
+    Fraction input;
+    char option;
+    string answer;
+
+    do  {
+        cout << "Enter a fraction type a/b with integer numbers: ";
+        cin >> input.numerator >> option >> input.denominator;
+        if (!cin || option != '/') {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please try again." << endl;
+        }
+        } while (!cin || option != '/');
+
+    answer = readFraction(input) ? "Valid fraction" : "Invalid fraction" ;
+    cout << answer << endl;
+
+    return 0;
+}
