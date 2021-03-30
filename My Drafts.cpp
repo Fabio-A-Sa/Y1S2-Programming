d79a56c9accc49098f7b10572eb1c5a646e4b4ee
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 17:14:19 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5159,3 +5159,85 @@ int main ()
 
     return 0;
 }
+
+struct Date {
+    unsigned int year, month, day;
+};
+
+void readDate (Date *d) {
+
+    char character;
+    cout << "Enter a date using format YYYY/MM/DD : ";
+    cin >> d->year >> character >> d->month >> character >> d->day ;
+}
+
+void writeDate(Date *d) {
+
+    cout << setfill('0') << setw(4) << d->year << "/" << setw(2) << d->month << "/" << setw(2) << d->day << endl;
+}
+
+int compareDates(const Date *d1, const Date *d2) {
+
+    if (d1->year < d2->year) {
+        return 1;
+    }
+    else if (d1->year > d2->year) {
+        return -1;
+    }
+    else if (d1->month < d2->month) {
+        return 1;
+    }
+    else if (d1->month > d2->month) {
+        return -1;
+    }
+    else if (d1->day < d2->day) {
+        return 1;
+    }
+    else if (d1->day > d2->day) {
+        return -1;
+    }
+    return 0;
+}
+
+void sortDates(Date *d1, Date *d2) {
+    
+    int sort = compareDates(d1, d2);
+    if (sort) {
+        if (sort < 0) {
+            writeDate(d2);
+            writeDate(d1);
+            cout << endl;
+        }
+        else {
+            writeDate(d1);
+            writeDate(d2);
+            cout << endl;
+        }
+    }
+    else {
+        cout << "Dates are equal: ";
+        writeDate(d1);
+    }
+}   
+
+int main () 
+{   
+    srand(time(NULL));
+    Date *data1, d1;
+    data1 = &d1;
+    cout << " " << endl;
+    readDate(data1);
+
+    Date *data2, d2;
+    data2 = &d2;
+    (*data2).day = rand() % 32;
+    (*data2).month = rand() % 13;
+    (*data2).year = rand() % 2022;
+
+    cout << "Random date: ";
+    writeDate(data2);
+    cout << " " << endl;
+    sortDates(data1, data2);
+
+    return 0;
+}
