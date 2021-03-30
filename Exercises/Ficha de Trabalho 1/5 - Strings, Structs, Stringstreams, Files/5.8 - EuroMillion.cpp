e017e25e2531dc749f98c991e23bcabad082d6f2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 15:51:19 +0100

    Added struct

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.8 - EuroMillion.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.8 - EuroMillion.cpp
@@ -5,7 +5,27 @@
 #include <vector>
 using namespace std;
 
+struct EuroMillionsBet {
+    vector<unsigned> mainNumbers;
+    vector<unsigned> luckStars;
+};
+
+void generate_random(EuroMillionsBet key) {
+
+
+}
+
 int main ()
 {
+    // Generate randoms key:
+    EuroMillionsBet random_key;
+    random_key.luckStars = {};
+    random_key.mainNumbers = {};
+    generate_random(random_key);
+
+    // Input users key:
+
+    
     return 0;
 }
+
