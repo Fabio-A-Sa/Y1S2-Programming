4368374e0abfa48b8763c357f3927fd127cab5c3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 16:04:51 +0100

    More

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.8 - EuroMillion.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.8 - EuroMillion.cpp
@@ -22,21 +22,41 @@ bool isInside (vector<unsigned> vector, unsigned int attemp) {
 
 void generate_random(EuroMillionsBet key) {
 
-
+    // Numbers (5)
+    int counter = 0;
+    while (counter != 5) {
+        unsigned int guess = rand() % 51;
+        if (!isInside(key.mainNumbers, guess)) {
+            key.mainNumbers.push_back(guess);
+            counter++;
+        }
+    }
+    // Lucky Stars (2)
+    counter = 0;
+    while (counter != 2) {
+        unsigned int guess = rand() % 13;
+        if (!isInside(key.luckStars, guess)) {
+            key.luckStars.push_back(guess);
+            counter++;
+        }
+    }
 }
 
 int main ()
 {
+    // Input users key:
+    EuroMillionsBet user_key;
+    int counter = 0;
+    while (counter != 5) {
+        
+    }
+
     // Generate randoms key:
     srand(time(NULL));
     EuroMillionsBet random_key;
     random_key.luckStars = {};
     random_key.mainNumbers = {};
     generate_random(random_key);
-
-    // Input users key:
-
-
     return 0;
 }
 
