0d8a555eea6c1cf41befb2f2ba9c5cbba460d07c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-30 20:04:04 +0100

    Test

---
+++ using files.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+	ifstream in_file("maze.txt");
+	if (!in_file.good()) {
+		cout << "Error opening file \"maze.txt\".\n";
+		exit(-1);
+	}
+
+    string aux;
+    cout << "\x1B[2J\x1B[H";
+	while (!in_file.eof())
+	{
+		getline(in_file, aux);
+		cout << aux << endl;
+	}
+	in_file.close();
+	return 0;
+}
