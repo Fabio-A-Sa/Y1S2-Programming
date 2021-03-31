974db3c6ff8a9b171eaef4baa691e844b0db46d6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-31 23:40:04 +0100

    Remove file

--- jknsf.py
+++
@@ -1,4 +0,0 @@
-astring = "         1         x        9                "
-alist = [int(x) for x in astring.split(" ") if x.lower() != "x" and x != ""]
-print(alist)
-
--- using files.cpp
+++ using files.cpp
@@ -22,6 +22,15 @@ int main ()
 
 	cout << "Numbers: " << lines << " " << cols << endl;
 
+	int i = 0;
+	string aux;
+	while (!in_file.eof())
+	{
+		getline(in_file, aux);
+		for (int j = 0; j < columns ; j++)
+			a[counter][j] = aux[j];
+		
+	}
 	in_file.close();
 	return 0;
 }
