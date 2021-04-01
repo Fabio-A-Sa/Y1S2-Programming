73128dfacfd33944abceedf824ce400ed0780963
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-01 09:10:22 +0100

    Begin

--- Notes/Theorical lecture 12.cpp
+++ Notes/Theorical lecture 12.cpp
@@ -1,3 +1,12 @@
 // Created on April 1, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 12, pages 131 --> ???
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
