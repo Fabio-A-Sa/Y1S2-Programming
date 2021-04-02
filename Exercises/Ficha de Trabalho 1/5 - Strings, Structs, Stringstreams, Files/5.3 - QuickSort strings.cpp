a3cc436f44b3c389fd08015def9132cf3ecf4256
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:08:03 +0100

    Descending order

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
@@ -7,7 +7,18 @@
 #include <string>
 using namespace std;
 
-int comparator ( const void *arg1, const void *arg2 ) {
+int comparator_d ( const void *arg1, const void *arg2 ) {
+
+   if ((*(char*)arg1 > *(char*)arg2)) {
+       return 1;
+   }
+   else if ((*(char*)arg1 < *(char*)arg2)) {
+       return -1;
+   }
+   return 0;
+}
+
+int comparator_a ( const void *arg1, const void *arg2 ) {
 
    if ((*(char*)arg1 > *(char*)arg2)) {
        return 1;
@@ -22,9 +33,19 @@ int main( int argc, char **argv )
 {
     char example[5] = {'f', 'a', 'b', 'i', 'o'} ;
 
-    qsort (example, 5, sizeof(char), comparator);    
-
-    for(int i = 0 ; i < 5 ; i++ )
+    //Ascending order
+    qsort (example, 5, sizeof(char), comparator_a);   
+    cout << "Ascending order: "; 
+    for (int i = 0 ; i < 5 ; i++ )
         cout << example[i] << " " ;
+    cout << endl;
 
+    // Descending order:
+    qsort (example, 5, sizeof(char), comparator_d); 
+    cout << "Descending order: ";
+    for (int j = 0 ; j < 5 ; j++ )
+        cout << example[j] << " ";
+    cout << endl;
+
+    return 0;
 }
