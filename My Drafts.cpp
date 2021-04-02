d442a9e2c450bb33c69e0a3f3a456535884ae69e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:27:53 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5531,3 +5531,20 @@ int main( int argc, char **argv )
 
     return 0;
 }
+
+bool isHidroxide (string compound) {
+
+    return (compound[compound.size()-2] == 'O' && compound[compound.size()-1] == 'H');
+}
+
+int main () 
+{   
+    vector<string> compounds = {"KOH", "H2O2", "NaCl", "NaOH", "C9H8O4", "MgOH"} ;
+    
+    for (string compound : compounds) {
+        string answer = isHidroxide(compound) ? " is hidroxide" : " is not hidroxide";
+        cout << compound << answer << endl;
+    }
+    
+    return 0;   
+}
