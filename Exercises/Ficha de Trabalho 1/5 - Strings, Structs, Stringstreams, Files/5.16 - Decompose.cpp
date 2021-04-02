20d13896abef3ca89f8def55528260b661028731
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:36:06 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.16 - Decompose.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.16 - Decompose.cpp
@@ -2,3 +2,14 @@
 // @author: Fábio Araújo de Sá
 
 // Taken from the previous years exercise Sheet
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+    vector<string> compounds = {"KOH", "H2O2", "NaCl", "NaOH", "C9H8O4", "MgOH"} ;
+    return 0;
+}
