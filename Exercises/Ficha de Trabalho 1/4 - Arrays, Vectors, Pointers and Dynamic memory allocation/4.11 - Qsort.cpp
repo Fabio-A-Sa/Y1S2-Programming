3b01ac1973387353501e0c5acfef606f4bef90e3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 11:53:38 +0100

    4.11

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
@@ -6,33 +6,55 @@
 #include <cstdlib>
 using namespace std;
 
-int comparator (const void* a, const void* b) {
+int comparator_a (const void* a, const void* b) {
 
     return (*(int*)a - *(int*)b);
 }
 
+int comparator_d (const void* a, const void* b) {
+
+    return (*(int*)b - *(int*)a);
+}
+
+
 int main ()
 {   
-    int example[10] = {9, 8, 5, 2, 7, 3, 1, 4, 0, -8} ;
+    int example[10];
+    int counter = 0;
+    int input;
+    while (counter != 10) {
+        cout << "Number " << counter + 1 << ": ";
+        cin >> input;
+        example[counter] = input;
+        counter++;
+    }
 
     char answer;
-    cout << "Ascending, descending or order? a/d/h: ";
+    cout << "Ascending, descending or half-half order? a/d/h: ";
     cin >> answer;
 
     switch (answer) {
-    case 'a':
-        qsort (example, 10, sizeof(int), comparator_a) ;
-        break;
-    case 'd':
-        qsort (example, 10, sizeof(int), comparator_d) ;
-        break;
-    case 'h':
-        qsort (example, 10, sizeof(int), comparator_a) ;
-        break;
+        case 'a':
+            qsort (example, 10, sizeof(int), comparator_a) ;
+            break;
+        case 'd':
+            qsort (example, 10, sizeof(int), comparator_d) ;
+            break;
+        case 'h':
+            qsort (example, 10, sizeof(int), comparator_a) ;
+            int aux1[5];
+            for (int i = 5 ; i < 10 ; i++) {
+                aux1[i-5] = example[i];
+            }
+            qsort (aux1, 5, sizeof(int), comparator_d) ;
+            for (int i = 5 ; i < 10 ; i++ ) {
+                example[i] = aux1[i-5];
+            }
+            break;
 
-    default:
-        cout << "Invalid input!" << endl;
-        exit(0);
+        default:
+            cout << "Invalid input!" << endl;
+            exit(0);
     }
 
     cout << "Sorted: ";
