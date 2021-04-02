5d537ce1b6d51fb876b7f7ef4dd7367fdbcc0f40
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:06:06 +0100

    Ascending order

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.3 - QuickSort strings.cpp
@@ -7,23 +7,24 @@
 #include <string>
 using namespace std;
 
-// Não funciona --> Verificar mais tarde!
-
 int comparator ( const void *arg1, const void *arg2 ) {
-   return  *arg1 - *arg2 ;
+
+   if ((*(char*)arg1 > *(char*)arg2)) {
+       return 1;
+   }
+   else if ((*(char*)arg1 < *(char*)arg2)) {
+       return -1;
+   }
+   return 0;
 }
 
 int main( int argc, char **argv )
 {
-    int arr[5] = {5, 8, 6, 4, 1} ;
-    int base = arr[0];
-    size_t size = 5;
-    size_t num;
+    char example[5] = {'f', 'a', 'b', 'i', 'o'} ;
 
-    qsort (void* base, size_t num, size_t size, int (*comparator)(const void*,const void*));    
+    qsort (example, 5, sizeof(char), comparator);    
 
-    // Print sorted data
-    for( i = 0 ; i < size ; ++i )
-        cout << arr[i] << " " ;
+    for(int i = 0 ; i < 5 ; i++ )
+        cout << example[i] << " " ;
 
 }
