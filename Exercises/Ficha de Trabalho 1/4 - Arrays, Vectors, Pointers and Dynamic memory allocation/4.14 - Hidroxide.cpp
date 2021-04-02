7997e9bae4c3560a11f00952885f82f0bff524d6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:17:22 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.14 - Hidroxide.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.14 - Hidroxide.cpp
@@ -1,2 +1,13 @@
 // Created on April, 2021
 // @author: Fábio Araújo de Sá
+
+// Taken from the previous years exercise Sheet
+
+#include <iostream>
+#include <array>
+#include <string>
+
+int main () 
+{
+    return 0;
+}
