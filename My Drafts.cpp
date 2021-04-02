6dbc931974412059de53e0b6b2266f49b5ce438b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:50:47 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5548,3 +5548,38 @@ int main ()
     
     return 0;   
 }
+
+void decompose (string compound) {
+    
+    string element = "";
+    for (char letter : compound)
+
+        if (islower(letter) && element.size()) {
+            element = element + letter;
+            cout << element << " ";
+            element = "";
+        }
+        else if (isupper(letter) && !element.size()) {
+            element = element + letter;
+        }
+        else if (isupper(letter) && element.size()) {
+            cout << element << " ";
+            element = "";
+            element = element + letter;
+        }
+        else
+            continue;
+
+    cout << element;
+}
+
+int main ()
+{
+    vector<string> compounds = {"KOH", "H2O2", "NaCl", "NaOH", "C9H8O4", "MgOH"} ;
+    for (string compound : compounds) {
+        cout << compound << " = ";
+        decompose(compound);
+        cout << endl;
+    }
+    return 0;
+}
