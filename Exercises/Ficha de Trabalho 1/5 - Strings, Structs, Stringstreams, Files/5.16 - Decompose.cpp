e7df7f49d030afabbb88d86fb9f2402c69ba0b01
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 12:50:03 +0100

    5.16

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.16 - Decompose.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.16 - Decompose.cpp
@@ -9,14 +9,34 @@
 using namespace std;
 
 void decompose (string compound) {
-    cout << "test";
+    
+    string element = "";
+    for (char letter : compound)
+
+        if (islower(letter) && element.size()) {
+            element = element + letter;
+            cout << element << " ";
+            element = "";
+        }
+        else if (isupper(letter) && !element.size()) {
+            element = element + letter;
+        }
+        else if (isupper(letter) && element.size()) {
+            cout << element << " ";
+            element = "";
+            element = element + letter;
+        }
+        else
+            continue;
+
+    cout << element;
 }
 
 int main ()
 {
     vector<string> compounds = {"KOH", "H2O2", "NaCl", "NaOH", "C9H8O4", "MgOH"} ;
     for (string compound : compounds) {
-        cout << compound << " ";
+        cout << compound << " = ";
         decompose(compound);
         cout << endl;
     }
