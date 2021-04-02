03e9fe041053058b620361c0bac28c59340ed271
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 11:35:35 +0100

    Refatorate code

--- Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
+++ Exercises/Ficha de Trabalho 1/4 - Arrays, Vectors, Pointers and Dynamic memory allocation/4.11 - Qsort.cpp
@@ -8,31 +8,16 @@ using namespace std;
 
 int comparator (const void* a, const void* b) {
 
-    if (a == b)
-        return 0;
-    else if (a < b) 
-        return 1;
-    else
-        return -1 ;
-}
-
-void qsort (void *base, size_t num, size_t size, int (*comparator)(const void*, const void*)) {
-
-    // Incomplete
-
+    return (*(int*)a - *(int*)b);
 }
 
 int main ()
 {   
     int example[10] = {9, 8, 5, 2, 7, 3, 1, 4, 0, -8} ;
-    int *base;
-    *base = example[0];
-    size_t num = 10;
-    size_t size = sizeof(example[0]);
 
-    qsort (*base, num, size, (*comparator)(void*, void*)) ;
+    cout << "Ascending or descending order? a/d: "
+    qsort (example, 10, sizeof(int), comparator) ;
 
-    // Show sorted data
     cout << "Sorted: ";
     for (int number : example) {
         cout << number << " ";
