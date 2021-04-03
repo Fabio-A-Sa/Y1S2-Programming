317ac6bbda2b1081020c1fa63a7627721008d179
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-03 11:21:11 +0100

    Modify structure

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.9 - Struct.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.9 - Struct.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream>
 using namespace std;
 
 struct Address {
@@ -20,11 +19,45 @@ struct Person {
     struct Address address;
 };
 
-void input_
+void input_data (vector<Person> &all_persons) {
+
+    char answer;
+
+    while (tolower(answer) != 'q') {
+
+        Person person;
+        cout << "Name: ";
+        cin >> person.name;
+        cout << "Age: ";
+        cin >> person.age;
+        cout << "Gender: ";
+        cin >> person.gender;
+        cout << "Street: ";
+        cin >> person.address.street;
+        cout << "Door number: ";
+        cin >> person.address.door_number;
+        cout << "City: ";
+        cin >> person.address.city;
+        all_persons.push_back(person);
+
+        cout << "Press 'Q' to quit or another letter to continue.";
+        cin >> answer;
+    }
+}
+
 int main ()
 {   
     vector<Person> all_persons;
+    input_data (all_persons);
+    
+    string city;
+    cout << "City to search: ";
+    cin >> city;
 
-
+    for (Person person : all_persons) {
+        if (person.address.city == city) {
+            cout << "This is a test" << endl;
+        };
+    }
     return 0;
 }
