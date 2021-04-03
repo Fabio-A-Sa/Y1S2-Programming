b1df7b869d75d8514da4564486eea2f7713a7c71
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-03 11:39:59 +0100

    Drafts update>

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5583,3 +5583,66 @@ int main ()
     }
     return 0;
 }
+
+struct Address {
+    string street;
+    int door_number;
+    string city;
+};
+
+struct Person {
+    string name;
+    int age;
+    char gender;
+    struct Address address;
+};
+
+void input_data (vector<Person> &all_persons) {
+
+    char answer;
+    struct Person Address;
+
+    while (answer != 'q' || answer != 'Q') {
+
+        Person person;
+        cout << "Name: ";
+        cin >> person.name;
+        cout << "Age: ";
+        cin >> person.age;
+        cout << "Gender: ";
+        cin >> person.gender;
+        cout << "Street: ";
+        cin >> person.address.street;
+        cout << "Door number: ";
+        cin >> person.address.door_number;
+        cout << "City: ";
+        cin >> person.address.city;
+        all_persons.push_back(person);
+
+        cout << "Press 'Q' to quit or another letter to continue.";
+        cin >> answer;
+    }
+}
+
+int main ()
+{   
+    vector<Person> all_persons;
+    vector<Person> same_city;
+    input_data (all_persons);
+    
+    string city;
+    cout << "City to search: ";
+    cin >> city;
+
+    for (Person person : all_persons) {
+        if (person.address.city == city) {
+            same_city.push_back(person);
+        };
+    }
+
+    for (Person person : same_city) {
+        cout << person.name << " is in " << person.address.street << " in " << city << endl;
+    }
+    
+    return 0;
+}
