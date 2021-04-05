32dab063f9de0823d64e13644029e4548c19eee5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 19:41:19 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.12 - Binary image.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.12 - Binary image.cpp
@@ -1,2 +1,13 @@
 // Created on April, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
