26dd83c9a3eef1b6d94de7ef4d306ee97431d7e6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 18:11:23 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.11 - Files to name.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.11 - Files to name.cpp
@@ -1,2 +1,13 @@
 // Created on April, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
