3eb0ffd335b1be451a7c39d0a5a64f6f78aed266
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 21:12:05 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5728,3 +5728,55 @@ int main ()
 
     return 0;
 }
+
+int main ()
+{   
+    vector<string> all_names;
+    string input, file_name;
+    string current_name;
+
+    cout << "File name: ";
+    cin >> input;
+    file_name = "Files\\" + input + ".txt";
+    ifstream file;
+    file.open(file_name);
+
+    // Verify user input
+    if (!file.good()) {
+
+        while (true) {
+
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "File not found. Please try again: ";
+            cin >> input;
+            file_name = "Files\\" + input + ".txt";
+            ifstream file(file_name);
+            if (file.good())
+                break;
+        }
+        file.open(file_name);
+    }
+
+    // Get data
+    while (!file.eof()) {
+        getline(file, current_name);
+        all_names.push_back(current_name);
+    }
+    file.close();
+
+    // Sort names using qsort algorithm
+    sort(all_names.begin(), all_names.end());
+
+    // Create new file and store sorted data, line by line
+    string new_file_name = input + "_sorted.txt";
+    ofstream another_file;
+    another_file.open(new_file_name);
+
+    for (string name : all_names)
+        another_file << name << endl;
+
+    another_file.close();
+
+    return 0;
+} 
