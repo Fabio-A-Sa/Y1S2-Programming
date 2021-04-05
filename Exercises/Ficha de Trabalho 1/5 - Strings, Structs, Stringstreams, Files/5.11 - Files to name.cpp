3f97eb45249c8283647f22ef48f657571ad909cb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 18:30:29 +0100

    Sorted names

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.11 - Files to name.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.11 - Files to name.cpp
@@ -4,10 +4,29 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
 #include <algorithm>
 using namespace std;
 
 int main ()
-{
+{   
+    vector<string> all_names;
+    string file_name = "Files\\names.txt";
+    string current_name;
+
+    ifstream file;
+    file.open(file_name);
+
+    while (!file.eof()) {
+        getline(file, current_name);
+        all_names.push_back(current_name);
+    }
+
+    file.close();
+    sort(all_names.begin(), all_names.end());
+
+    for (string name : all_names) {
+        cout << name << endl;
+    }
     return 0;
-}
+} 
