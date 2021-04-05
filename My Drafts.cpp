ac4bfd7d52b54ab67d0b10178efcaeb037422d76
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 17:27:09 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -5584,25 +5584,47 @@ int main ()
     return 0;
 }
 
-struct Address {
-    string street;
-    int door_number;
-    string city;
-};
-
 struct Person {
+
     string name;
     int age;
     char gender;
-    struct Address address;
-};
+    struct Address {
+
+        string street;
+        int door_number;
+        string city;
+    };
+
+    Address address;
+} ;
+
+bool notInVector (vector<string> vec , string attemp) {
+    
+    for (string item : vec) {
+        if (item == attemp) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void remove_duplicates(vector<string> &streets) {
+
+    vector<string> without_duplicates;
+    for (string street : streets) {
+        if (notInVector(without_duplicates, street)) {
+            without_duplicates.push_back(street);
+        }
+    }
+    streets = without_duplicates;
+}
 
 void input_data (vector<Person> &all_persons) {
 
     char answer;
-    struct Person Address;
 
-    while (answer != 'q' || answer != 'Q') {
+    while (answer != 'q' && answer != 'Q') {
 
         Person person;
         cout << "Name: ";
@@ -5628,6 +5650,7 @@ int main ()
 {   
     vector<Person> all_persons;
     vector<Person> same_city;
+    vector<string> all_streets;
     input_data (all_persons);
     
     string city;
@@ -5637,12 +5660,21 @@ int main ()
     for (Person person : all_persons) {
         if (person.address.city == city) {
             same_city.push_back(person);
+            all_streets.push_back(person.address.street);
         };
     }
-
-    for (Person person : same_city) {
-        cout << person.name << " is in " << person.address.street << " in " << city << endl;
-    }
     
+    remove_duplicates(all_streets);
+
+    cout << "City: " << city << endl;
+    for (string street : all_streets) {
+        cout << "Street: " << street << endl;
+        for (Person person : same_city) {
+            if (person.address.street == street) {
+                cout << "- " << person.name << endl;        
+            }
+        }
+    }
+
     return 0;
 }
