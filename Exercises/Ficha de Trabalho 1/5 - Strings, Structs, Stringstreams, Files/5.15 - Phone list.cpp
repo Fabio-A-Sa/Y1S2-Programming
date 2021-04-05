6143ce4259dcc798d13101a1ffb1ea1ba5803ff4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 21:20:20 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.15 - Phone list.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.15 - Phone list.cpp
@@ -1,2 +1,22 @@
 // Created on April, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void create_random_phone_list () {
+
+    string file_name = "random_phone_list.txt";
+    ifstream file;
+    file.open(file_name);
+
+    while
+}
+int main ()
+{   
+    create_random_phone_list(file_name, )
+    return 0;
+}
