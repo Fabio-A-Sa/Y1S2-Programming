487debfc1d57f7ca90b9199bcbb0e41280bc354d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 21:00:09 +0100

    Compressed image

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.12 - Binary image.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.12 - Binary image.cpp
@@ -22,7 +22,31 @@ int main ()
     }
     file.close();
 
-    cout << cols << " " << lines << " " << content << endl;
+    int counter;
+    char current_letter;
+    string compressed_content, part;
+    string current_stack = "";
+    current_stack += content[0];
+
+    for (int index = 1 ; index < content.size() ; index ++ ) {
+
+        current_letter = content[index];
+
+        if ( current_stack[current_stack.size()-1] == current_letter ) {
+            current_stack += current_letter;
+        }
+        else {
+            counter = current_stack.size() ;
+            part = current_letter == 'b' ? to_string(counter) + 'w' : to_string(counter) + 'b' ;
+            compressed_content += part;
+            current_stack = "";
+            current_stack += current_letter;
+        }
+    }
+    part = current_stack[current_stack.size()] == 'b' ? to_string(current_stack.size()) + 'w' : to_string(current_stack.size()) + 'b';
+    compressed_content += part;
+
+    cout << compressed_content << " " << compressed_content.size();
 
     return 0;
 }
