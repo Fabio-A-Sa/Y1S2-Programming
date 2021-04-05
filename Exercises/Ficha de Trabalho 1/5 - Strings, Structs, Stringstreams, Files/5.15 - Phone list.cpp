04005bd0ee2feaab2e68e5897f6c556a4f036a3a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-05 21:48:37 +0100

    Added files

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.15 - Phone list.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.15 - Phone list.cpp
@@ -5,18 +5,44 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <ctime>
 using namespace std;
 
-void create_random_phone_list () {
+void create_random_phone_list (string name, int value) {
 
-    string file_name = "random_phone_list.txt";
-    ifstream file;
-    file.open(file_name);
+    ofstream new_file;
+    new_file.open(name);
+    ifstream names_file;
+    names_file.open("random_names.txt");
 
-    while
+    vector<string> all_names;
+    string current_name;
+    while (!names_file.eof())
+
+    while (value) {
+
+        string number = "";
+        number += '9';
+        while (number.size() < 9) {
+            int digit = rand() % 10;
+            number += to_string(digit);
+        }
+        new_file << number << endl;
+        value--;
+    }
+    
+    new_file.close();
 }
+
 int main ()
-{   
-    create_random_phone_list(file_name, )
+{       
+    srand(time(NULL));
+    int value;
+    string file_name = "random_phone_list.txt";
+    cout << "How many numbers? ";
+    cin >> value;
+
+    create_random_phone_list(file_name, value);
+
     return 0;
 }
