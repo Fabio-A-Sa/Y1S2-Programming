6e25093672df135af81f082ac9bacf93c297987b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-06 14:11:53 +0100

    Added functions

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.13 - EuroMillions Players.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.13 - EuroMillions Players.cpp
@@ -6,9 +6,47 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
+
+struct User {
+
+    vector<unsigned> numbers;
+    string name;
+};
+
+bool notInside(vector<unsigned> v, unsigned int value) {
+
+    bool flag = true;
+    for (unsigned int index = 0 ; index < v.size() ; index++ ) {
+        flag = flag && (v[index] != value) ;
+    }
+    return flag;
+}
+
+void generate_random(vector<unsigned> &key) {
+
+    int counter = 0;
+    while (counter != 5) {
+        unsigned int guess = rand() % 51;
+        if (notInside(key, guess)) {
+            key.push_back(guess);
+            counter++;
+        }
+    }
+
+    counter = 0;
+    while (counter != 2) {
+        unsigned int guess = rand() % 13;
+        if (notInside(key, guess)) {
+            (key).push_back(guess);
+            counter++;
+        }
+    }
+}
 
 int main ()
 {
+
     return 0;
 }
