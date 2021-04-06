32072c9af0cd714ccd10f5ea73fae0c591d5fa7e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-06 14:02:48 +0100

    Begin

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.13 - EuroMillions Players.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.13 - EuroMillions Players.cpp
@@ -1,2 +1,14 @@
 // Created on April, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
