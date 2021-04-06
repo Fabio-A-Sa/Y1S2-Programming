610403e12338cc868f48f47cd55210b023f65b9a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-06 13:40:31 +0100

    Added functions

--- Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.14 - StringStreams.cpp
+++ Exercises/Ficha de Trabalho 1/5 - Strings, Structs, Stringstreams, Files/5.14 - StringStreams.cpp
@@ -6,7 +6,29 @@
 #include <vector>
 using namespace std;
 
+bool isAllowed(string word) {
+    
+    vector<string> denied = {"DE", "DO", "DA", "DOS", "DAS", "E"} ;
+    bool flag = true;
+    for (string attemp : denied) {
+        flag = flag && (attemp != word);
+    }
+    return flag;
+}
+
+string to_upper (string str) {
+
+    for (int index = 0 ; index < str.size() ; index++ ) {
+        str[index] = toupper(str[index]);
+    }
+    return str;
+}
+
 int main ()
 {
+    string name;
+    cout << "Enter a name: ";
+    getline(cin, name);
+    cout << "Normalization: " << normalizeName(name) << endl;
     return 0;
 }
