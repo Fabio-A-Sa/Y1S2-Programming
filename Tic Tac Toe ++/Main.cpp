4a79772143ab8a90620c0dd97cefedb702af14b6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-07 17:05:08 +0100

    Chose a symbol

--- Tic Tac Toe ++/Main.cpp
+++ Tic Tac Toe ++/Main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <string>
+#include <limits>
 #include <cstddef>
 
 using namespace std;
+
+struct Player {
+
+    string name;
+    char symbol;
+};
 
 void fill (char *board) {
 
@@ -23,6 +31,34 @@ void printBoard (char *board) {
     cout << "#############" << endl;
 }
 
+void choose_options(vector<Player> &players) {
+
+    string name;
+    char symbol;
+
+    Player player1;
+    cout << "Player 1 Name: ";
+    getline(cin, name);
+    player1.name = name;
+    cout << "Choose a symbol: 'X' or 'O' : ";
+    cin >> symbol;
+    player1.symbol = symbol;
+    players.push_back(player1);
+    
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    Player player2;
+    cout << "Player 2 Name: ";
+    getline(cin, name);
+    player2.name = name;
+    symbol = players[0].symbol == 'X' ? 'O' : 'X';
+    cout << "Your symbol is obviously '" << symbol << "'." << endl;
+    player2.symbol = symbol;
+    players.push_back(player2);
+
+}
+
 int main () 
 {   
     // Dinamic memory allocation using malloc() method
@@ -30,13 +66,14 @@ int main ()
     char * board = (char *) malloc (positions * sizeof(char));
 
     fill(board);
-    printBoard(board);
+    vector<Player> players;
+    choose_options(players);
 
-    for (int i : {1, 2}) {
-        cout << i << endl;
+    for (Player player : players) {
+        cout << player.name << " " << player.symbol << endl;
     }
+    printBoard(board);
 
     free(board);
     return 0;
 }
-
