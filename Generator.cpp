7cfbfbafba1f70ab450c0bade592dfb22ab24026
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-09 12:08:23 +0100

    Test

--- Generator.cpp
+++ Generator.cpp
@@ -5,13 +5,36 @@
 #include <fstream>
 
 #define MIN_COLS 5;
-#define MIN_COLS 5;
+#define MIN_LINES 5;
 #define FENCE '*';
 #define PLAYER 'H';
 #define ROBOT 'R';
 
-
 using namespace std;
+
+int random (int max) 
+{   
+    int number = rand() % (max + 1) + MIN_COLS ;
+    return number;
+}
+
+void generate (int max_cols, int max_lines, int quantity) 
+{
+    int cols = random (max_cols);
+    int lines = random (max_lines);
+    int area = lines * cols ;
+    vector<char> maze = {};
+
+    // Input white spaces
+    while (area) 
+    {   
+        maze.push_back(' ');
+        area--;
+    }
+
+    // Input border
+    
+}
 
 int main ()
 {   
