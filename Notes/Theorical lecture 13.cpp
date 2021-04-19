cd707b42e46bb750272ca0c5a4cd54c69cae33c4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-19 17:06:03 +0100

    Added header

--- Notes/Theorical lecture 13.cpp
+++ Notes/Theorical lecture 13.cpp
@@ -1,3 +1,12 @@
 // Created on April 19, 2021
 // @author: Fábio Araújo de Sá
-// Theorical lecture 13, pages 144 --> 1??
+// Theorical lecture 13, pages 144 --> 162
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
