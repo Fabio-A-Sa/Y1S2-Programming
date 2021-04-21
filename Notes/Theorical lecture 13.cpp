05c99f8ad2234023030dd1073900a649c4437c06
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-21 17:14:01 +0100

    Add function

--- Notes/Theorical lecture 13.cpp
+++ Notes/Theorical lecture 13.cpp
@@ -18,9 +18,11 @@ void open_file () {
     string current_line;
     new_file.open(directory);
     if (new_file.good()) {
-        while (!new_file.eof())
+        while (!new_file.eof()) {
             getline(new_file, current_line);
             cout << current_line;
+        }
+        new_file.close();
     }
     else {
         cout << "This file does not exist. Please try again.";
