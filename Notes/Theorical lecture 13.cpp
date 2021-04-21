d04de051eae6e6d2c8aba045b891b435b01c8a41
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-21 17:06:27 +0100

    Added includes

--- Notes/Theorical lecture 13.cpp
+++ Notes/Theorical lecture 13.cpp
@@ -3,9 +3,18 @@
 // Theorical lecture 13, pages 144 --> 162
 
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
+void open_file () { 
+
+    string directory;
+    cout << "Directory of file: ";
+    getline(cin, directory);
+
+}
 int main ()
 {
     return 0;
