a16cec9ececf0ab8e9f70cd324177f1561efd0b9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-22 09:42:10 +0100

    Begin

--- Notes/Theorical lecture 14.cpp
+++ Notes/Theorical lecture 14.cpp
@@ -1,3 +1,13 @@
 // Created on April 22, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 14, pages 166 --> 1??
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
