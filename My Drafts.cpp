2bff0a3c0f2fcae6b65f4455395f22a3d75538fd
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-22 10:38:37 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -6237,3 +6237,49 @@ void open_file () {
         cout << "This file does not exist. Please try again.";
     }
 }
+
+const int MAX_NAMELEN = 10;
+
+typedef struct {
+    char name[MAX_NAMELEN];
+    unsigned int age;
+} Person;
+
+void using_text_files () {
+
+    char c;
+    string name = "text.txt";
+    fstream file;
+    file.open(name);
+
+    // using cin >>
+    file.seekg(0, ios::beg); // Pointer to the beginning of file
+    cout << "File contents usign cin: " << endl;
+    while (file >> c)
+    {
+        cout << c;
+    }
+    cout << endl;
+    file.clear(); // Clear eof() flag
+
+    //using get();
+    file.seekg(0, ios::beg);
+    cout << "File contents using get: " << endl;
+    while (file.get(c))
+    {
+        cout << "Next letter: " << c << endl;
+    }
+    cout << endl;
+    file.clear();
+    file.close();
+}
+
+void using_binary_files () {
+    
+    int place = 1;
+    fstream personFile;
+    Person a[MAX_NAMELEN];
+    personFile.open("names.dat", ios::out | ios::binary);
+    personFile.seekg(place);
+    personFile.write((char *) a, 10*sizeof(Person));
+}
