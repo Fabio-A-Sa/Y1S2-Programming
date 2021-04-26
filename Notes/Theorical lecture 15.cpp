d34a0092a044f1bf24135975ac1feaa80d5cd86b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-26 11:27:44 +0100

    Begin

--- Notes/Theorical lecture 15.cpp
+++ Notes/Theorical lecture 15.cpp
@@ -1,3 +1,16 @@
 // Created on April 26, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 15, pages 170 --> ???
+
+#include <iostream>
+#include <string>
+#include <isstring
+
+void extraction() {
+    string input
+}
+int main ()
+{
+    extraction();
+    return 0;
+}
