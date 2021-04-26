41cc6d83205fb5f9431e252836da688d5a28145b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-26 11:43:13 +0100

    Strings to numbers

--- Notes/Theorical lecture 15.cpp
+++ Notes/Theorical lecture 15.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 using namespace std;
 
 void normalization() {
@@ -13,14 +14,25 @@ void normalization() {
     string month, comma;
     int day, year;
 
+    cout << "Normalization of "  << input << " is: ";
     istringstream istr(input);
     istr >> month >> day >> comma >> year;
     cout << month << " " << day << comma << " " << year << endl;
 }
 
+void strings_to_numbers() {
+
+    cout << "Strings to numbers: \n";
+    vector<string> numbers = {" 89 ", "   78", "  -1   ", "72    "};
+    for (string number : numbers) {
+        cout << stoi(number) << endl;
+    }
+}
+
 int main ()
 {
     normalization();
+    strings_to_numbers();
     return 0;
 }
 
