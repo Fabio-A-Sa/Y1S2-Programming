41ff3cabb92a143b1daf946040946ebc871c0f0c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-04 15:51:14 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/2 - Students.cpp
+++ Exercises/Ficha de Trabalho 2/2 - Students.cpp
@@ -1,2 +1,33 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+    
+    public:
+
+        Student();
+        Student(const string &code, const string &name);
+        void setGrades(double shortExam, double project, double exam);
+        string getCode() const;
+        string getName() const;
+        int getFinalGrade() const;
+        bool isApproved() const;
+        static int weightShortExam, weightProject, weightExam;
+
+    private:
+
+        string code;
+        string name;
+        double shortExam, project, exam;
+        int finalGrade;
+
+} ;
+
+int main()
+{
+    return 0;
+}
