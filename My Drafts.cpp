281446ee19396e5ea25e0aaa8b2aeaf7548933ca
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-06 10:44:07 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -6786,3 +6786,33 @@ int main ()
 
     return 0;
 }
+
+template <class T>
+void swapvalues (T x, T y) {
+
+    cout << "Before: x = " << x << " and y = " << y << endl;
+    T temp = x;
+    x = y;
+    y = temp;
+    cout << "After: x = " << x << " and y = " << y << endl;
+}
+
+void using_iterators() {
+
+    vector<int> numbers = {5, 51, 51, 6, 51, 65, 53, 8, 52, 51, 96, 14, 541};
+    vector<int>::iterator p;
+    for (p = numbers.begin() ; p != numbers.end() ; p++ ) {
+        cout << *p << endl;
+    }
+}
+
+int main ()
+{
+    swapvalues("Hello", "World");  // Strings
+    swapvalues(56165, 651651);     // Integers
+    swapvalues('F', 'A');          // Chars
+
+    using_iterators();
+
+    return 0;
+}
