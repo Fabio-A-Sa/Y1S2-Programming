d72d01f6d5b671aab382aafcdcbeec83fcafea87
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-06 10:43:25 +0100

    Added lecture's notes

--- Notes/Theorical lecture 18.cpp
+++ Notes/Theorical lecture 18.cpp
@@ -1,6 +1,6 @@
 // Created on May 06, 2021
 // @author: Fábio Araújo de Sá
-// Theorical lecture 18, pages 220 --> ???
+// Theorical lecture 18, pages 220 --> 251
 
 #include <iostream>
 #include <string>
@@ -17,17 +17,22 @@ void swapvalues (T x, T y) {
     cout << "After: x = " << x << " and y = " << y << endl;
 }
 
-int main ()
-{
-    swapvalues("Hello", "World");  // Strings
-    swapvalues(56165, 651651);     // Integers
-    swapvalues('F', 'A');          // Chars
+void using_iterators() {
 
     vector<int> numbers = {5, 51, 51, 6, 51, 65, 53, 8, 52, 51, 96, 14, 541};
     vector<int>::iterator p;
     for (p = numbers.begin() ; p != numbers.end() ; p++ ) {
         cout << *p << endl;
     }
-    
+}
+
+int main ()
+{
+    swapvalues("Hello", "World");  // Strings
+    swapvalues(56165, 651651);     // Integers
+    swapvalues('F', 'A');          // Chars
+
+    using_iterators();
+
     return 0;
 }
