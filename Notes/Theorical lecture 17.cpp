5218c8cfb67bd27b95102f03861a6d6beacafef4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-06 09:13:38 +0100

    Added number

--- Notes/Theorical lecture 17.cpp
+++ Notes/Theorical lecture 17.cpp
@@ -1,6 +1,6 @@
 // Created on May 03, 2021
 // @author: Fábio Araújo de Sá
-// Theorical lecture 17, pages 195 --> ???
+// Theorical lecture 17, pages 195 --> 220
 
 #include <iostream>
 #include <string>
