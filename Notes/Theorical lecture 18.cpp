88107c92d8f3d10bf905a372da0c32ea9475653a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-06 09:14:32 +0100

    Begin

--- Notes/Theorical lecture 18.cpp
+++ Notes/Theorical lecture 18.cpp
@@ -1,3 +1,12 @@
 // Created on May 06, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 18, pages 220 --> ???
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
