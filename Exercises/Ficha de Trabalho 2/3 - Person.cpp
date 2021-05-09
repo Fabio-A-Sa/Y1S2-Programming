22d12b176bee5e645d089b6167189102fee24099
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-09 14:16:42 +0100

    Added classes

--- Exercises/Ficha de Trabalho 2/3 - Person.cpp
+++ Exercises/Ficha de Trabalho 2/3 - Person.cpp
@@ -1,7 +1,40 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
 
+class Date {
 
+    public: 
+        Date();
+        Date(int day, int month, int year);
+        int getDay();
+        int getYear();
+        int getMonth();
+        void showData();
+
+    private:
+        int day;
+        int month;
+        int year;
+};
+
+Date::Date(int d, int m, int year) {
+    day = d;
+    month = m;
+    year = y;
+}
+
+int Date::getDay() {
+    return day;
+}
+class Person {
+
+    public:    
+
+
+
+    private:
+
+};
 
 int main ()
 {
