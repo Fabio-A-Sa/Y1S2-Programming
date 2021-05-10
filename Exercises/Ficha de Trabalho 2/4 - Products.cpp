ddd9eb039ae7b1572fbd9d0f0d0b4045cfd71783
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-10 15:56:12 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/4 - Products.cpp
+++ Exercises/Ficha de Trabalho 2/4 - Products.cpp
@@ -1,3 +1,17 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class {
+    
+};
+
+int main ()
+{
+    return 0;
+}
