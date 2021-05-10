c8e0f0096d395eb893220ae1ce2167ed632cbf31
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-10 12:36:27 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -7116,3 +7116,61 @@ int main ()
     show_data(all_persons);
     return 0;
 }
+
+void using_iterator() {
+
+    vector<int>::reverse_iterator p; // --> or using "auto" in for loop
+    vector<int> numbers;
+
+    // Input random numbers in vector
+    for (int i = 0 ; i < 100 ; i+= rand() % 20) {
+        numbers.push_back(i); 
+    }
+    // Iterator in reverse order
+    for (p = numbers.rbegin() ; p != numbers.rend() ; p++ ) {
+        cout << "Number: " << *p << endl;
+    }
+}
+
+void sort_algorithm() {
+
+    // Fill vector
+    vector<int> numbers;
+    while (numbers.size() < 20) {
+        numbers.push_back(rand() % 100);
+    }
+    cout << "\nNot sorted: ";
+    for (int number : numbers) {
+        cout << number << " ";
+    }
+    // Sort by ascending order
+    sort(numbers.begin(), numbers.end());
+    cout << "\nSorted: ";
+    for (int number : numbers) {
+        cout << number << " ";
+    }
+}
+
+void using_map() {
+
+    map<int, string> phone_user;
+    vector<string> names = {"Fabio", "Ines", "Pedro", "Malva", "Carolina", "Manel"};
+
+    // Fill map, "dictionary in Python"
+    for (int i = 0 ; i < 10 ; i++) {
+        phone_user.insert(pair<int, string> (rand() % 100000000, names[rand() % 6]));
+    }
+
+    // Return values
+    for (auto x : phone_user) {
+        cout << x.first << " - " << x.second << endl;
+    }
+}
+
+int main ()
+{
+    using_iterator();
+    sort_algorithm();
+    using_map();
+    return 0;
+}
