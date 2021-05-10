f4a48f1121f8bb07ccdfe23b1516fa0e7755eed5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-10 16:00:33 +0100

    Create Client class

--- Exercises/Ficha de Trabalho 2/4 - Products.cpp
+++ Exercises/Ficha de Trabalho 2/4 - Products.cpp
@@ -7,9 +7,27 @@
 #include <vector>
 using namespace std;
 
-class {
-    
+class Client {
+
+    public:
+        Client();
+        Client(string name, string adress);
+        string getAdress();
+        string getName();
+
+    private:
+        string name;
+        string adress;
+
 };
+
+Client::Client(string name, string adress) {
+    this -> name = name;
+    this -> adress = adress;
+}
+
+string Client::getName() { return name; }
+string Client:: getAdress() { return adress; }
 
 int main ()
 {
