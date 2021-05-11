86fb84488953bacc02fba2576ebab2a67b00495b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-11 15:48:52 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/5 - Library.cpp
+++ Exercises/Ficha de Trabalho 2/5 - Library.cpp
@@ -1,3 +1,23 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+typedef unsigned long IdentNum;
+
+class Book {
+
+    public:
+        Book();
+        Book(string bookName);
+        void setName(string bookName);
+        IdentNum getId() const;
+
+};
+
+int main() 
+{
+    return 0;
+}
