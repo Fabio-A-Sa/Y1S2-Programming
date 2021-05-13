120dc8c6f1a5370fb9e83277d90aa0283606f259
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-13 09:40:09 +0100

    More

--- Notes/Theorical lecture 20.cpp
+++ Notes/Theorical lecture 20.cpp
@@ -5,9 +5,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 
+void count_many_words() {
+
+    map <string, unsigned int> m;
+    typedef map <string, unsigned int>::const_iterator MapIterator;
+    string word;
+    
+    cout << "Write a text; end with <ENTER> followed by <CTRL-Z>\n";
+    while(cin >> word) {
+        m[word]++;
+    }
+
+    for (MapIterator i = m.begin(); i != m.end(); i++) {
+        cout << i->first << ": "<< i->second << endl;
+    }
+}
+
 int main ()
-{
+{   
+    count_many_words();
     return 0;
 }
