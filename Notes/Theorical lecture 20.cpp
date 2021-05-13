4911170d41942cec875294cc7dcb6f43f12e2ba6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-13 09:34:16 +0100

    Begin

--- Notes/Theorical lecture 20.cpp
+++ Notes/Theorical lecture 20.cpp
@@ -2,3 +2,12 @@
 // @author: Fábio Araújo de Sá
 // Theorical lecture 20, pages 268 --> ???
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
