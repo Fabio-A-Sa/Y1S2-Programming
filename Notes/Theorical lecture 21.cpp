b5b91688cd4ad5a0fca1cdc52ff7132736e31b32
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-17 11:22:47 +0100

    Beginm

--- Notes/Theorical lecture 21.cpp
+++ Notes/Theorical lecture 21.cpp
@@ -1,3 +1,11 @@
 // Created on May 17, 2021
 // @author: Fábio Araújo de Sá
 // Theorical lecture 21, pages 285 --> ???
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
