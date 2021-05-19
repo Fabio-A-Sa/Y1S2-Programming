299f5f624c1cb0da0cc965c576c654e7468fa414
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 20:52:59 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/9 - Sort.cpp
+++ Exercises/Ficha de Trabalho 2/9 - Sort.cpp
@@ -1,2 +1,24 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void using_my_algorithm() {
+
+}
+
+void using_STL()  {
+
+}
+
+int main ()
+{
+    using_my_algorithm();
+    using_STL();
+    return 0;
+}
