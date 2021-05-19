67fdf168aa2235366f6a44089f090b5bf1fe8aae
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 19:02:59 +0100

    Vector class#

--- Exercises/Ficha de Trabalho 2/8 - Vectors.cpp
+++ Exercises/Ficha de Trabalho 2/8 - Vectors.cpp
@@ -5,6 +5,30 @@
 #include <string>
 using namespace std;
 
+template <class T>
+
+class Vector
+{
+    public:
+
+        Vector();
+        Vector(unsigned int size);
+        Vector(unsigned int size, const T &initial);
+        Vector(const Vector<T> &v);
+        ~Vector();
+
+        int size() const;
+        void push_back(const T &value);
+        void pop_back();
+        void clear();
+
+    private:
+
+        T * buffer;
+        int bufferSize;
+        int size;
+};
+
 int main ()
 {
     return 0;
