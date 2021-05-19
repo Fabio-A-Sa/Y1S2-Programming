9b740fefbc2d21510e4fa0a667af6106a5164deb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 17:26:04 +0100

    begin

--- Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
+++ Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
@@ -1,2 +1,21 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+class Product
+{
+    public:
+
+    private:
+};
+
+class Inventory
+{
+    public:
+
+    private:
+};
+
+int main()
+{
+    return 0;
+}
