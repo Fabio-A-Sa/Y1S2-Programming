1bc6eff12ca4620d42c7349124cf83cd672d0df1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 21:29:23 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/10 - List.cpp
+++ Exercises/Ficha de Trabalho 2/10 - List.cpp
@@ -1,2 +1,74 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <fstream>
+using namespace std;
+
+bool inList (list<string> lst, string name) {
+
+    for (auto attemp : lst) {
+        if (attemp == name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void using_my_algorithm() {
+
+    string current_file = "Files\\Names.txt", new_file = "Files\\Sorted.txt";
+    vector<string> all_names = {}, new_names = {};
+    ifstream file;
+    file.open(current_file);
+    
+    if (file.good()) {
+        
+        string current_name;
+        while(!file.eof()) {
+    
+            getline (file, current_name);
+            all_names.push_back(current_name);
+        }
+
+        file.close();
+        
+        for (auto name : all_names) {
+            if (!inVector(new_names, name)) {
+                new_names.push_back(name);
+            }
+        }
+
+        sort(new_names.begin(), new_names.end());
+
+    }
+    else {
+        cout << "Wrong file!" << endl;
+    }
+    
+    if (new_names.size()) {
+
+        ofstream file; 
+        file.open(new_file);
+
+        for (auto word : new_names) {
+            file << word << endl;
+        }
+        
+        file.close();
+    }
+
+    else {
+        cout << "Wrong file!" << endl;
+    }
+
+}
+
+int main ()
+{
+    using_my_algorithm();
+    return 0;
+}
