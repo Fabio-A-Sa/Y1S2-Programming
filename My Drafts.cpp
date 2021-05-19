c70d2e803cbb088c1d6ba883539241ac38f8696c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 18:53:46 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -7402,3 +7402,28 @@ int main()
     test();
     return 0;
 }
+
+template<class T>
+
+void max_and_min(T v) {
+
+    int max = -100000;
+    int min = 100000;
+
+    for (int i : v) {
+        if (i > max) {
+            max = i;
+        }
+        if (i < min) {
+            min = i;
+        }
+    }
+    cout << "Min: " << min << " and Max: " << max << endl;
+}
+
+int main()
+{   
+    vector<int> v = {-101, 2, 356, 0, 7, 567, -96};
+    max_and_min(v);
+    return 0;
+}
