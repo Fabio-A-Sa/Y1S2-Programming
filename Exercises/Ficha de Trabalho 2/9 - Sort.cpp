3fd410df3dca9083ebf07bb9d89d5591a93f3d9c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 20:57:49 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/9 - Sort.cpp
+++ Exercises/Ficha de Trabalho 2/9 - Sort.cpp
@@ -10,6 +10,40 @@ using namespace std;
 
 void using_my_algorithm() {
 
+    string current_file = "Files\\Names.txt", new_file = "Files\\Sorted.txt";
+    vector<string> all_names;
+    ifstream file;
+    file.open(current_file);
+    
+    if (file.good()) {
+        
+        string current_name;
+        while(!file.eof()) {
+    
+            getline (file, current_name);
+            all_names.push_back(current_name);
+        }
+
+        file.close();
+
+
+    }
+    else {
+        cout << "Wrong file" << endl;
+    }
+    
+    if (all_names.size()) {
+
+        ofstream file; 
+        file.open(new_file);
+
+        for (auto word : all_names) {
+            file << word << endl;
+        }
+        
+        file.close();
+    }
+
 }
 
 void using_STL()  {
