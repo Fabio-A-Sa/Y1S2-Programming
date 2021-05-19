56414ea5fae35f0c5d50af0f538615a693cf34b5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 22:07:12 +0100

    Add class#

--- Exercises/Ficha de Trabalho 2/12 - Euromillion.cpp
+++ Exercises/Ficha de Trabalho 2/12 - Euromillion.cpp
@@ -4,7 +4,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
+
+vector<int> main_numbers, lucky_stars;
+
+void initialize() {
+    
+    for (int number = 1 ; number < 51 ; number ++) {
+        main_numbers.push_back(number);
+    }
+    for (int number = 1 ; number < 13 ; number ++) {
+        lucky_stars.push_back(number);
+    }
+    random_shuffle(main_numbers.begin(), main_numbers.end());
+
+}
 
 class Bet
 {
@@ -19,7 +34,18 @@ class Bet
         vector<int> stars;
 };
 
+Bet::Bet() {
+    main = {};
+    stars = {};
+    fill_numbers();
+}
+
+void Bet::fill_numbers() {
+    
+}
+
 int main ()
-{
+{   
+    initialize();
     return 0;
 }
