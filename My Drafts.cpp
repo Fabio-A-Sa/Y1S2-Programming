22a5ffe1d6cb5c2ea1cc582a4f56e394b8449a8b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 21:32:26 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -7550,3 +7550,66 @@ int main ()
     using_my_algorithm();
     return 0;
 }
+
+bool inList (list<string> lst, string name) {
+
+    for (auto attemp : lst) {
+        if (attemp == name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void using_my_algorithm() {
+
+    string current_file = "Files\\Names.txt", new_file = "Files\\Sorted.txt";
+    list<string> all_names = {}, new_names = {};
+    ifstream file;
+    file.open(current_file);
+    
+    if (file.good()) {
+        
+        string current_name;
+        while(!file.eof()) {
+    
+            getline (file, current_name);
+            all_names.push_back(current_name);
+        }
+
+        file.close();
+        
+        for (auto name : all_names) {
+            if (!inList(new_names, name)) {
+                new_names.push_back(name);
+            }
+        }
+
+    }
+    else {
+        cout << "Wrong file!" << endl;
+    }
+    
+    if (new_names.size()) {
+
+        ofstream file; 
+        file.open(new_file);
+
+        for (auto word : new_names) {
+            file << word << endl;
+        }
+        
+        file.close();
+    }
+
+    else {
+        cout << "Wrong file!" << endl;
+    }
+
+}
+
+int main ()
+{
+    using_my_algorithm();
+    return 0;
+}
