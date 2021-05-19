cde873dc4ae0661c30f7676e4eee8491ed1899d0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 17:42:35 +0100

    begin

--- Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
+++ Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
@@ -11,42 +11,79 @@ class Product
     public:
 
         Product();
-        Product(int id, int quantity, string description);
+        Product(int quantity, string description);
         int getId();
         int getQuantity();
         string getDescription();
 
     private:
 
+        static int identify;
         int id;
         int quantity;
         string description;
 };
 
+int Product::identify = 1;
+
 Product::Product() {
-    id = 0;
+    identify++;
+    id = identify;
     quantity = 0;
-    string description = "";
+    string description = "UNKNOWN";
 }
 
-Product::Product(int id, int quantity, string description) {
-    this -> id = id;
+Product::Product(int quantity, string description) {
+    identify++;
+    id = identify;
     this -> quantity = quantity;
     this -> description = description;
 }
 
-int Product::getId() {return id; }
-int Product::getQuantity() {return quantity; }
-string Product::getDescription() {return description; }
+int Product::getId() { return id; }
+int Product::getQuantity() { return quantity; }
+string Product::getDescription() { return description; }
 
 class Inventory
 {
     public:
 
+        Inventory();
+        vector<Product> getInventory();
+        void includeProduct(Product product);
+        void showInventory();
+
     private:
+
+        vector<Product> products;
+
 };
+
+Inventory::Inventory() {
+    products = {};
+}
+
+vector<Product> Inventory::getInventory() {
+    return products;
+}
+
+void Inventory::includeProduct(Product product) {
+    products.push_back(product);
+}
+
+void Inventory::showInventory() {
+    for (auto product : products) {
+        cout << "Product: " << product.getDescription() << " and quantity: " << product.getQuantity() << endl;
+    }
+}
+
+void test()
+{
+    
+}
 
 int main()
 {
+    test();
     return 0;
 }
