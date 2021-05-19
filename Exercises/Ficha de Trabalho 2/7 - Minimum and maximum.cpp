a7f2c25119950fb4ea495c991f428ffffd3efceb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 18:47:22 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/7 - Minimum and maximum.cpp
+++ Exercises/Ficha de Trabalho 2/7 - Minimum and maximum.cpp
@@ -5,9 +5,26 @@
 #include <vector>
 using namespace std;
 
+template<class T>
 
+void max_and_min(T v) {
+    int max = -100000;
+    int min = 100000;
+
+    for (int i : v) {
+        if (i > max) {
+            max = i;
+        }
+        if (i < min) {
+            min = i;
+        }
+    }
+    cout << "Min: " << min << " and Max: " << max << endl;
+}
 
 int main()
-{
+{   
+    vector<int> v = {1, 2, 3};
+    max_and_min(v);
     return 0;
 }
