bbf8291cdf737c98f886005522bc4cd2e4190792
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 22:26:24 +0100

    Using set

--- Exercises/Ficha de Trabalho 2/12 - Euromillion.cpp
+++ Exercises/Ficha de Trabalho 2/12 - Euromillion.cpp
@@ -27,6 +27,7 @@ class Bet
 
         Bet();
         void fill_numbers();
+        void show_numbers();
     
     private:
 
@@ -44,19 +45,36 @@ void Bet::fill_numbers() {
     
     random_shuffle(main_numbers.begin(), main_numbers.end());
     random_shuffle(lucky_stars.begin(), lucky_stars.end());
-
+    
     for (int i = 0 ; i < 5 ; i++ ) {
         main.push_back(main_numbers[i]);
     }
+
+    // Using lucky_stars vector
     for (int j = 0 ; j < 2 ; j++ ) {
         stars.push_back(lucky_stars[j]);
     }
 }   
 
+void Bet::show_numbers() {
+    
+    cout << "Main numbers: ";
+    for (int number : main) {
+        cout << number << " ";
+    }
+    cout << endl << "Lucky numbers: ";
+    for (int number : stars) {
+        cout << number << " ";
+    }
+}
+
 int main ()
 {   
     srand(time(NULL));
     initialize();
+
+    Bet b1;
+    b1.show_numbers();
 
     return 0;
 }
