e067983755994c75eb60aab5dc0cb82b1bc30873
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-19 17:28:55 +0100

    begin

--- Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
+++ Exercises/Ficha de Trabalho 2/6 - Inventory.cpp
@@ -1,12 +1,31 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
 class Product
 {
     public:
 
+        Product();
+        Product(int id, int quantity, string description);
+        int getId();
+        int getQuantity();
+        string getDescription();
+
     private:
+
+        int id;
+        int quantity;
+        string description;
 };
+
+Product::Product() {
+
+}
 
 class Inventory
 {
