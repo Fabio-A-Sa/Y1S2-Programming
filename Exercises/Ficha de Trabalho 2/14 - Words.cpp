9dcf8120ef72c8346e88db57754bc0701691858d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 14:03:09 +0100

    Read file begin

--- Exercises/Ficha de Trabalho 2/14 - Words.cpp
+++ Exercises/Ficha de Trabalho 2/14 - Words.cpp
@@ -14,15 +14,30 @@ void store (vector<vector<string>> &data) {
     file.open(file_name);
     if (file.good()) {
         
+        while (!file.eof()) {
+            string current_line;
+            vector<string> current_content = {};
+            getline(file, current_line);
+            for (int i = 0 ; i < current_line.size() ; i++ ) {
+                char current_char = current_line[i];
+
+            }
+            data.push_back(current_content);
+        }
     }   
     else {
         cout << "Error!" << endl;
     }
 }
 
+void print_data (vector<vector<string>> data) {
+    
+}
+
 int main ()
 {   
     vector<vector<string>> all_data;
     store(all_data);
+    print_data(all_data);
     return 0;
 }
