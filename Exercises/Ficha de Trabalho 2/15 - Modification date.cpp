f4d660e6ee0f895806ca1f063ca6e91fc242419c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 15:02:31 +0100

    Added friend functions

--- Exercises/Ficha de Trabalho 2/15 - Modification date.cpp
+++ Exercises/Ficha de Trabalho 2/15 - Modification date.cpp
@@ -189,6 +189,57 @@ void Date::show() const {
     cout << result << endl;
 }
 
+bool operator < (Date date1, Date date2) {
+
+    if (date1.getYear() != date2.getYear())  {
+        return date1.getYear() < date2.getYear();
+    }
+    else {
+        if (date1.getMonth() != date2.getMonth()) {
+            return date1.getMonth() < date2.getMonth();
+        }
+        else {
+            if (date1.getDay() != date2.getDay()) {
+                return date1.getDay() < date2.getDay();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+bool operator > (Date date1, Date date2) {
+    
+    if (date1.getYear() != date2.getYear())  {
+        return date1.getYear() > date2.getYear();
+    }
+    else {
+        if (date1.getMonth() != date2.getMonth()) {
+            return date1.getMonth() > date2.getMonth();
+        }
+        else {
+            if (date1.getDay() != date2.getDay()) {
+                return date1.getDay() > date2.getDay();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+bool operator != (Date date1, Date date2) {
+    return date1.getDate() != date2.getDate();
+}
+
+bool operator <= (Date date1, Date date2);
+    friend bool operator >= (Date date1, Date date2);
+    friend bool operator == (Date date1, Date date2);
+    friend bool operator << (Date date1, Date date2);
+    friend bool operator >> (Date date1, Date date2);
+
+
 int main ()
 {
     return 0;
