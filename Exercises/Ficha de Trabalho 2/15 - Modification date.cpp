d0645ae2a97da88b4abdea197d5c7d8058679551
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 14:48:16 +0100

    Copy class

--- Exercises/Ficha de Trabalho 2/15 - Modification date.cpp
+++ Exercises/Ficha de Trabalho 2/15 - Modification date.cpp
@@ -6,6 +6,180 @@
 #include <string>
 using namespace std;
 
+class Date
+{
+    public:
+        Date();
+        Date(unsigned int year, unsigned int month,unsigned int day);
+        Date(string yearMonthDay);
+        void setYear(unsigned int year) ;
+        void setMonth(unsigned int month) ;
+        void setDay(unsigned int day) ;
+        void setDate(unsigned int year, unsigned int month, unsigned int day) ;
+        unsigned int getYear() const;
+        unsigned int getMonth() const;
+        unsigned int getDay() const;
+        string getDate() const;
+        void show() const;
+
+        bool isValid();
+        bool isEqualTo(const Date &date);
+        bool isNotEqualTo(const Date &date);
+        bool isAfter(const Date &date);
+        bool isBefore(const Date &date);
+
+    private:
+
+        unsigned int year;
+        unsigned int month;
+        unsigned int day;
+
+        unsigned int how_many_days(unsigned year, unsigned month);
+        bool isLeapYear (unsigned int year);
+}; 
+
+Date::Date()
+{
+    year = 0;
+    month = 0;
+    day = 0;
+}
+
+bool Date::isEqualTo(const Date &date) {
+    return (day == date.getDay() && month == date.getMonth() && year == date.getYear());
+}
+
+bool Date::isNotEqualTo(const Date &date) {
+    return !isEqualTo(date);
+}
+
+bool Date::isAfter(const Date &date) {
+
+    bool flag = true;
+
+    if (year > date.getYear()) {
+        flag = false;
+    }
+    else {
+        if (month > date.getMonth()) {
+            flag = false;
+        }
+        else {
+            if (day > date.getDay()) {
+                flag = false;
+            }
+            else {
+                if (isEqualTo(date)) {
+                    flag = false;
+                }
+            }
+        }
+    }
+    return !flag;
+}   
+
+bool Date::isBefore(const Date &date) {
+    return ! (isAfter(date) || isEqualTo(date));
+}
+
+bool Date::isValid() {
+
+    bool answer;
+    answer = ( day > 0 && day <= how_many_days(year, month) && month < 13 && month > 0 && year >= 0 && year < 2022 );
+    return answer;
+}
+
+Date::Date(unsigned int y, unsigned int m, unsigned int d) {
+    year = y;
+    month = m;
+    day = d;
+}
+
+Date::Date(string yearMonthDay) {
+    int y, m, d;
+    char x;
+    istringstream str(yearMonthDay);
+    str >> y >> x >> m >> x >> d;
+    day = d;
+    month = m;
+    year = y;
+}
+
+unsigned int Date::how_many_days(unsigned year, unsigned month) {
+
+    unsigned int days = 0;
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        days = 31;
+        break;
+    
+        case 4: case 6: case 9: case 11:
+        days = 30;
+        break;
+
+        case 2:
+            if (Date::isLeapYear(year)) {
+                days = 29;
+            }
+            else {
+                days = 28;
+            }
+            break;
+    }
+    return days;
+}
+
+bool Date::isLeapYear (unsigned int year) {
+
+    bool flag = true;
+    flag = flag && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ;
+    return flag;
+}
+
+void Date::setYear(unsigned int y) {
+    year = y;
+}
+
+void Date::setMonth(unsigned int m) {
+    month = m;
+}
+
+void Date::setDay(unsigned int d) {
+    day = d;
+}
+
+void Date::setDate(unsigned int y, unsigned int m, unsigned int d) {
+    year = y;
+    month = m;
+    day = d;
+}
+
+unsigned int Date::getYear() const {
+    return year;
+}
+
+unsigned int Date::getMonth() const {
+    return month;
+}
+
+unsigned int Date::getDay() const {
+    return day;
+}
+
+string Date::getDate() const {
+    string all;
+    string d = to_string(day); while (d.size() < 2) { d = "0" + d ; } ;
+    string m = to_string(month); while (m.size() < 2) { m = "0" + m ; } ;
+    string y = to_string(year); while (y.size() < 4) {y = "0" + y ; } ;
+    all = y + "/" + m + "/" + d;
+    return all;
+}
+
+void Date::show() const {
+    string result = getDate();
+    cout << result << endl;
+}
+
 int main ()
 {
     return 0;
