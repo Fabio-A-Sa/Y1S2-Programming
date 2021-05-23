d57afeff4c413eaa527f952f07a9e37823509f37
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 20:37:54 +0100

    Added example

--- Exercises/Ficha de Trabalho 2/16 - Iterator.cpp
+++ Exercises/Ficha de Trabalho 2/16 - Iterator.cpp
@@ -4,9 +4,16 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
+
+template<class FowardIterator, class UniaryPredicate>
+FowardIterator remove_if(FowardIterator first, FowardIterator last, UniaryPredicate pred);
+
+
 
 int main ()
 {
+    vector<int> example = {-2, 3, 5, -1, 4, -39, -92, -2, 0, -5, 4, 92, -3, 0, 1};
     return 0;
 }
