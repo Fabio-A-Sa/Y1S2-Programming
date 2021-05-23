2f603cb9afb89201e743eb412dfb9dcb5eb7c23c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 20:44:33 +0100

    18 possible

--- Exercises/Ficha de Trabalho 2/18 - FEUP person.cpp
+++ Exercises/Ficha de Trabalho 2/18 - FEUP person.cpp
@@ -7,6 +7,34 @@
 #include <string>
 using namespace std;
 
+class FEUP_person {
+
+    public:
+
+        FEUP_person();
+        FEUP_person(string name, int upCode);
+        int getUpCode();
+        string getName();
+
+    private:
+
+        string name;
+        int upCode;
+};
+
+FEUP_person::FEUP_person() {
+    name = "";
+    upCode = 0;
+}
+
+FEUP_person::FEUP_person(string name, int upCode) {
+    this -> name = name;
+    this -> upCode = upCode;
+}
+
+string FEUP_person::getName() { return name; }
+int FEUP_person::getUpCode() { return upCode; }
+
 int main ()
 {
     return 0;
