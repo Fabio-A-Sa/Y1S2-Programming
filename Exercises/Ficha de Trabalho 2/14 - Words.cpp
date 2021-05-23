5ef5233d1ea13948b93821745a32895d740e282c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 14:00:05 +0100

    Read file begin

--- Exercises/Ficha de Trabalho 2/14 - Words.cpp
+++ Exercises/Ficha de Trabalho 2/14 - Words.cpp
@@ -3,10 +3,26 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
 using namespace std;
 
+void store (vector<vector<string>> &data) {
+
+    string file_name = "Files\\Text.txt";
+    ifstream file;
+    file.open(file_name);
+    if (file.good()) {
+        
+    }   
+    else {
+        cout << "Error!" << endl;
+    }
+}
+
 int main ()
-{
+{   
+    vector<vector<string>> all_data;
+    store(all_data);
     return 0;
 }
