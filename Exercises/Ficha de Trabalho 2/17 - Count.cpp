b1e05b73bfa151d27d1d14af5dcc94e26dbbb125
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 20:17:43 +0100

    Using namespace my

--- Exercises/Ficha de Trabalho 2/17 - Count.cpp
+++ Exercises/Ficha de Trabalho 2/17 - Count.cpp
@@ -1,2 +1,21 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace myfuncs {
+    void cout(const string &s);
+    bool endl(const string &s);
+}
+
+void myfuncs::cout(const string &s) {
+
+}
+
+int main ()
+{
+    std::cout << "Test";
+    return 0;
+}
