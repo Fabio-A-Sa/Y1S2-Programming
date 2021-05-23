dcac39bebd0dec05883284d8bfcc70f5a4432bee
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 13:48:04 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -8003,3 +8003,43 @@ int main ()
     
     return 0;
 }
+
+class Vector {
+    public:
+
+        Vector();
+        Vector(unsigned int size);
+
+        int size();
+        void push_back(int value);
+        void pop_back();
+        void clear();
+
+    private:
+
+        int bufferSize;
+};
+
+Vector::Vector() {
+    bufferSize = 0;
+}
+
+Vector::Vector(unsigned int size) {
+    this -> bufferSize = size;
+
+}
+
+void Vector::clear() {
+    bufferSize = 0;
+}
+
+int Vector::size() {
+    return bufferSize;
+}
+
+int main ()
+{
+    Vector v = Vector(10);
+    v.clear();
+    return 0;
+}
