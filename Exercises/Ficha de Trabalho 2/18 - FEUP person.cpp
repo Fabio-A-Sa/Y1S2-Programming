7eb260b374e8be2b3636e52385521f00803b574f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 20:39:57 +0100

    Begin

--- Exercises/Ficha de Trabalho 2/18 - FEUP person.cpp
+++ Exercises/Ficha de Trabalho 2/18 - FEUP person.cpp
@@ -1,2 +1,13 @@
 // Created on May, 2021
 // @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
