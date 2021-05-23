430e0ee7100e05232924c064d64278b26364f0d9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 13:47:42 +0100

    8

--- Exercises/Ficha de Trabalho 2/8 - Vectors.cpp
+++ Exercises/Ficha de Trabalho 2/8 - Vectors.cpp
@@ -2,56 +2,44 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
-#include <string>
-#include <array>
 using namespace std;
-
-template<typename T>
 
 class Vector {
     public:
 
         Vector();
         Vector(unsigned int size);
-        Vector(unsigned int size, const T &initial);
-        Vector(const Vector<T> &v);
-        ~Vector();
 
-        int size() const;
-        void push_back(const T &value);
+        int size();
+        void push_back(int value);
         void pop_back();
         void clear();
 
     private:
 
-        T * buffer;
         int bufferSize;
-        int size;
 };
 
 Vector::Vector() {
-    size = 0;
     bufferSize = 0;
 }
 
 Vector::Vector(unsigned int size) {
-    this -> size = size;
+    this -> bufferSize = size;
 
 }
 
-Vector::Vector(unsigned int size, const T &initial) {
-    this -> size = size;
-    this -> buffer = initial;
+void Vector::clear() {
+    bufferSize = 0;
 }
 
-Vector::Vector(const Vector<T> &v) {
-    this -> buffer = v;
+int Vector::size() {
+    return bufferSize;
 }
 
 int main ()
 {
     Vector v = Vector(10);
-    v.push_back(5);
     v.clear();
     return 0;
 }
