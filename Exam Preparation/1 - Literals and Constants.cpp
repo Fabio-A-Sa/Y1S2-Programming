68bfedbca05e99cd11d46ee706b332758ea5e0dd
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 19:13:11 +0100

    Drive

--- Exam Preparation/1 - Literals and Constants.cpp
+++ Exam Preparation/1 - Literals and Constants.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main ()
 {
-    // Some types of numbers in C++ notation
+    // Some types of numbers in C++ notation:
     int number1 = 11;
     float number2 = 10.3;
     int number3 = 0x58;
@@ -16,6 +16,7 @@ int main ()
     int number6 = 89L;
     int number7 = 8214234LL;
     double number8 = 2e-1;
+    int number9 = 5.3E3;
     cout << "This is an integer number: " << number1 << endl;
     cout << "This is a double/float number: " << number2 << endl;
     cout << "This is a hexadecimal number: " << number3 << endl;
@@ -24,6 +25,13 @@ int main ()
     cout << "This is a long number: " << number6 << endl;
     cout << "This is a long long int number: " << number7 << endl;
     cout << "This is a real number: " << number8 << endl;
+    cout << "This is another real number: " << number9 << endl;
 
+    // Some transformations:
+    char letter = 'F';
+    char number = 100;
+    cout << "Letter " << letter << " to decimal number: " << (int) letter << endl;
+    cout << "Number " << number << " to letter: " << (char) number << endl;
+    
     return 0;
 }
