27f623b27a7b69fca1cc8291dcc504e8fb078c5e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 19:25:26 +0100

    Default options

---
+++ Exam Preparation/3 - IoManip.cpp
@@ -0,0 +1,12 @@
+// Created on June, 2021
+// @author: Fábio Araújo de Sá
+// Page 16 of Lectures Material
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
