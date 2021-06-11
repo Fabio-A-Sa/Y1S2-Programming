529952ba5f60c87ba15af9c42a7dfa3692807b67
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 21:23:44 +0100

    Call by value

--- Exam Preparation/4 - Statements.cpp
+++ Exam Preparation/4 - Statements.cpp
@@ -1,9 +1,12 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
-// Page 20 of Lectures Material
+// Page 20/46 of Lectures Material
 
 #include <iostream>
 using namespace std;
+
+void by_value(int number) { number = 23987; }
+void by_reference (int &number) { number = 23987; }
 
 int main()
 {   
@@ -18,6 +21,16 @@ int main()
             break; // Stop a loop, without print i < 12
         }
         cout << i << endl;
-    }
+    }   
+
+    // Call by value and vall by reference
+    int X = 10;
+    cout << "\nDefault: " << X << " ";
+    by_value(X);
+    cout << "\nCall by value: " << X << " ";
+    by_reference(X);
+    cout << "\nCall by reference: " << X << " ";
+
+    
     return 0;
 }
