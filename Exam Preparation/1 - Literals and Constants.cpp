4a50120aa997c32581f913c2d2a5f69f31d5db02
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 19:06:18 +0100

    Addede lines

--- Exam Preparation/1 - Literals and Constants.cpp
+++ Exam Preparation/1 - Literals and Constants.cpp
@@ -10,9 +10,20 @@ int main ()
     // Some types of numbers in C++ notation
     int number1 = 11;
     float number2 = 10.3;
+    int number3 = 0x58;
+    int number4 = 054;
+    int number5 = 75U;
+    int number6 = 89L;
+    int number7 = 8214234LL;
+    double number8 = 2e-1;
     cout << "This is an integer number: " << number1 << endl;
     cout << "This is a double/float number: " << number2 << endl;
-
+    cout << "This is a hexadecimal number: " << number3 << endl;
+    cout << "This is an octal number: " << number4 << endl;
+    cout << "This is an unsigned number: " << number5 << endl;
+    cout << "This is a long number: " << number6 << endl;
+    cout << "This is a long long int number: " << number7 << endl;
+    cout << "This is a real number: " << number8 << endl;
 
     return 0;
 }
