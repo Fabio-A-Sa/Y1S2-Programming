2fdc12f1df847e8f4a942cfd7edb459ddbdec2f0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 20:05:54 +0100

    Continue and break statements

--- Exam Preparation/4 - Statements.cpp
+++ Exam Preparation/4 - Statements.cpp
@@ -9,7 +9,13 @@ int main()
 {   
     for (int i = 0 ; i < 101 ; i++ ) {
         if (i == 13) {
-            continue;
+            continue; // Jump to another number, without print '13'
+        }
+        cout << i << endl;
+    }
+    for (int i = 0 ; i < 101 ; i++ ) {
+        if (i == 13) {
+            break; // Stop a loop, without print i < 12
         }
         cout << i << endl;
     }
