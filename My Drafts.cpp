98b505b1676d76f91b764cbce5a78684ff0487af
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 21:38:55 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -8526,3 +8526,33 @@ int main ()
     cout << fixed << setfill('F') << setprecision(10) << name1 << name2 << endl;
     return 0;
 }
+
+void by_value(int number) { number = 23987; }
+void by_reference (int &number) { number = 23987; }
+
+int main()
+{   
+    for (int i = 0 ; i < 101 ; i++ ) {
+        if (i == 13) {
+            continue; // Jump to another number, without print '13'
+        }
+        cout << i << endl;
+    }
+    for (int i = 0 ; i < 101 ; i++ ) {
+        if (i == 13) {
+            break; // Stop a loop, without print i < 12
+        }
+        cout << i << endl;
+    }   
+
+    // Call by value and vall by reference
+    int X = 10;
+    cout << "\nDefault: " << X << " ";
+    by_value(X);
+    cout << "\nCall by value: " << X << " ";
+    by_reference(X);
+    cout << "\nCall by reference: " << X << " ";
+
+    
+    return 0;
+}
