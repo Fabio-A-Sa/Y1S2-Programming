6bc4ce270922924e643283edb10e7ad5d29981cf
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 20:15:45 +0100

    Drive

--- Exam Preparation/5 - Invalid inputs and buffer.cpp
+++ Exam Preparation/5 - Invalid inputs and buffer.cpp
@@ -3,9 +3,33 @@
 // Page 21 of Lectures Material
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
-{
+{   
+    vector<int> numbers;
+    cout << "Input 10 numbers: ";
+    int counter = 0;
+    int current_number;
+
+    while (numbers.size() != 10) {
+        cout << "Number " << counter+1 << ": ";
+        cin >> current_number;
+        if (cin) {
+            numbers.push_back(current_number);
+            cin.ignore(10000, '\n');
+            counter+=1;
+        }
+        else {
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+
+    for (auto number : numbers) {
+        cout << number << " ";
+    }
+
     return 0;
 }
