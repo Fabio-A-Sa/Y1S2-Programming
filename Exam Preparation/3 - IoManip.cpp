22a0fba279b7b273ad2c22c2125018c562b3a321
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 20:02:17 +0100

    Added file

--- Exam Preparation/3 - IoManip.cpp
+++ Exam Preparation/3 - IoManip.cpp
@@ -13,11 +13,11 @@ int main ()
     cout << "Two numbers: ";
     cin >> number1 >> number2;
     cout << fixed << setprecision(10) << (double) number1 / number2 << endl;
-    cout << setprecision(10) << (double) number1 / number2 << endl;
+    cout << setprecision(5) << (double) number1 / number2 << endl;
 
     string name1, name2;
     cout << "Two names: ";
     cin >> name1 >> name2;
-    cout << fixed << setprecision(10) << name1 << name2 << endl;
+    cout << fixed << setfill('F') << setprecision(10) << name1 << name2 << endl;
     return 0;
 }
