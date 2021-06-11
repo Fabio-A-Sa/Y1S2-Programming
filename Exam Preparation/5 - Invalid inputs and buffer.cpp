fc8696f1983610d46cd0d89859f232a0250dc375
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 20:31:30 +0100

    Using example

--- Exam Preparation/5 - Invalid inputs and buffer.cpp
+++ Exam Preparation/5 - Invalid inputs and buffer.cpp
@@ -9,16 +9,22 @@ using namespace std;
 
 void to_do() {
 
-    int current_number;
+    // Tested with "#ABcdE12345$Esc" string
+    char current_char;
     cout << "Chars: ";
-    while (cin >> current_number) {
-        if (cin) {
-            cout << current_number;
+    while (cin >> current_char) {
+
+        if (cin.peek() != '\n') {
+            if (isdigit(current_char)) {
+                cout << current_char;
+            }
+            cin.clear();
         }
         else {
-            continue;
+            break;
         }
     }
+    cout << endl;
 }
 
 int main()
@@ -46,8 +52,9 @@ int main()
     for (auto number : numbers) {
         cout << number << " ";
     }
+    cout << endl;
 
-    to_do();
+    to_do(); // Example in page 22
 
     return 0;
 }
