39ca70343250e33d617a4a54b3547572d9eec22e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-11 18:53:48 +0100

    Exam Preparation

--- Exam Preparation/1 - Literals and Constants.cpp
+++ Exam Preparation/1 - Literals and Constants.cpp
@@ -1,6 +1,6 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
-// Page ??? of Lectures Material
+// Page 8 of Lectures Material
 
 #include <iostream>
 using namespace std;
@@ -10,7 +10,9 @@ int main ()
     // Some types of numbers in C++ notation
     int number1 = 11;
     float number2 = 10.3;
-    
-    cout << "This is an integer" << number1 << endl;
+    cout << "This is an integer number: " << number1 << endl;
+    cout << "This is a double/float number: " << number2 << endl;
+
+
     return 0;
 }
