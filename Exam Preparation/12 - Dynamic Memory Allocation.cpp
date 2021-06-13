d06d15984769949d92db470c53d9be840ef8d022
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 16:34:58 +0100

    2D array

--- Exam Preparation/12 - Dynamic Memory Allocation.cpp
+++ Exam Preparation/12 - Dynamic Memory Allocation.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 void using_C () {
 
+    // 1D array
     int n = 25, *p;
     p = (int *) malloc(n * sizeof(int));
     for (int i = 0 ; i < n ; i++ ) {
@@ -20,10 +21,33 @@ void using_C () {
     }
     cout << endl;
     free(p);
+
+    // 2D array
+    int ncol = 3, nline = 5, **matrix;
+    matrix = (int **) malloc (nline * sizeof(int *));
+    for (int i = 0 ; i < nline ; i++ ) {
+        matrix[i] = (int *) malloc (ncol * sizeof(int));
+    }
+
+    for (int i = 0 ; i < nline ; i++) {
+        for (int j = 0 ; j < ncol ; j++) {
+            matrix[i][j] = i+j;
+        }
+    }
+    
+    for (int i = 0 ; i < nline ; i++) {
+        for (int j = 0 ; j < ncol ; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    free(matrix);
 }
 
 void using_CPP () {
 
+    // 1D array
     int n = 25;
     int *p = new int[n];
     
