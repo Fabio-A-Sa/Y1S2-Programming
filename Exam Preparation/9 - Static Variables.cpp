ad65b8a4ab1efed46a80074cb81f822ccfb19e43
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 12:43:19 +0100

    Static values

---
+++ Exam Preparation/9 - Static Variables.cpp
@@ -0,0 +1,20 @@
+// Created on June, 2021
+// @author: Fábio Araújo de Sá
+// Page 64 of Lectures Material
+
+#include <iostream>
+using namespace std;
+
+int getTicketNumber() {
+
+    static int ticketNum = 0; // initialized only once, at program startup
+    ticketNum++;
+    return ticketNum;
+}
+int main()
+{
+    int i;
+    for (i=1; i <= 10; i++)
+    cout << "ticket number = " << getTicketNumber() << endl;
+    return 0;
+}
