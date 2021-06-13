8072a1492920bc7cb483dcf0bd0cd76d5c7518bb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 14:56:48 +0100

    Pointers, again

--- Exam Preparation/07 - Pointers.cpp
+++ Exam Preparation/07 - Pointers.cpp
@@ -1,15 +1,15 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
-// Page 47 of Lectures Material
+// Page 47/102 of Lectures Material
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    // Y is memory-copy of X. If X is changed, Y is changed too.
     int x;
-    int &y = x; // Y is memory-copy of X. If X is changed, Y is changed too.
-
+    int &y = x;
     x = 10;
     cout << "x = " << x << "; y = " << y << endl;
     y = 205654;
@@ -17,7 +17,15 @@ int main()
     x = 10;
     cout << "x = " << x << "; y = " << y << endl;
 
-
+    // Pointer operators
+    // & --> Returns the memory adress of its operand
+    int example = 10;
+    int *p1 = &example;
+    cout << "& example: " << p1 << endl;
     
+    // * --> Returns the value of number in adress *p1
+    int another = *p1;
+    cout << another;
+
     return 0;
 }
