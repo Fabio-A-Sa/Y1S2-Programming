c62e87a309d2a9aa747cef9c6de6525824ad024c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 16:44:24 +0100

    Using strings

--- Exam Preparation/06 - Strings.cpp
+++ Exam Preparation/06 - Strings.cpp
@@ -1,9 +1,10 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
-// Page 27 of Lectures Material
+// Page 27/123 of Lectures Material
 
 #include <iostream>
 #include <string>
+#include <array>
 using namespace std;
 
 int main()
@@ -18,5 +19,10 @@ int main()
     for (auto c : example) {
         cout << (char) toupper(c);
     }
+
+    cout << "String in C: " << endl;
+    char s[] = "fabiosa";
+    cout << "String size is: " << sizeof(s) / sizeof(s[0]) << endl; // String.size() + 1 --> '\n'
+
     return 0;
 }
