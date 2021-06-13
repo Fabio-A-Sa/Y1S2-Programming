09f7aff0cf1f774e9d6860e1ff7691bd64ef9dd7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 16:17:35 +0100

    Memory

--- Exam Preparation/12 - Dynamic Memory Allocation.cpp
+++ Exam Preparation/12 - Dynamic Memory Allocation.cpp
@@ -4,11 +4,12 @@
 
 #include <iostream>
 #include <array>
+#include <new> // For using new and delete
 #include <cstdlib> // For using malloc() and free() methods
 using namespace std;
 
 void using_C () {
-    
+
     int n = 25, *p;
     p = (int *) malloc(n * sizeof(int));
     for (int i = 0 ; i < n ; i++ ) {
@@ -23,11 +24,24 @@ void using_C () {
 
 void using_CPP () {
 
+    int n = 25;
+    int *p = new int[n];
+    
+    for (int i = 0 ; i < n ; i++ ) {
+        *(p+i) = i;
+    }
+
+    for (int i = 0 ; i < n ; i++ ) {
+        cout << *(p+i) << " ";
+    }
+    cout << endl;
+    delete [] p;
 }
 
 int main ()
 {
     // Using C and C++ methods
     using_C();
+    using_CPP();
     return 0;
 }
