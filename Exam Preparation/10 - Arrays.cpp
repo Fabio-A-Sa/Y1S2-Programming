60c50d00cc0f259c8e3083b4efc9d909d308a52e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 13:00:32 +0100

    Added header

--- Exam Preparation/10 - Arrays.cpp
+++ Exam Preparation/10 - Arrays.cpp
@@ -1,3 +1,12 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
 // Page 77 of Lectures Material
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
