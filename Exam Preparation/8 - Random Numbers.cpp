6bab1a218116750257edcc60eb49906efe4f580b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 12:31:49 +0100

    Drafts

--- Exam Preparation/8 - Random Numbers.cpp
+++ Exam Preparation/8 - Random Numbers.cpp
@@ -1,3 +1,20 @@
 // Created on June, 2021
 // @author: Fábio Araújo de Sá
 // Page 52 of Lectures Material
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main ()
+{   
+    srand(time(NULL));
+    int counter = 0;
+    int min = 100, max = 98123;
+
+    while (counter != 10) {
+        cout << min + (rand() % (max-min));
+    }
+
+    return 0;
+}
