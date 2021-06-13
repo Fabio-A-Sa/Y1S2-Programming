a0fa9fea63030be22fc3506298a2ed2d2aba86a5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 18:08:48 +0100

    Drafts

--- Exam Preparation/13 - Structs.cpp
+++ Exam Preparation/13 - Structs.cpp
@@ -3,9 +3,33 @@
 // Page 142 of Lectures Material
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
 using namespace std;
 
+struct FEUP {
+
+    string location;
+    int students;
+    double price;
+};
+
 int main ()
-{
+{   
+    // Normal
+    FEUP faculty;
+    faculty.location = "Rua Torcato Gomes";
+    faculty.students = 4300;
+    faculty.price = 697.00;
+    cout << faculty.location << " " << faculty.price << " " << faculty.students << endl;
+
+    // Using pointers
+    FEUP * f;
+    f = (FEUP *) malloc(sizeof(FEUP));
+    f->location = "Rua da Silva";
+    f->students = 3287;
+    f->price = 700;
+    cout << f->price << " " << f->price << " " << f->price << endl; // Note: *p.location make an error!
+    free(f);
     return 0;
 }
