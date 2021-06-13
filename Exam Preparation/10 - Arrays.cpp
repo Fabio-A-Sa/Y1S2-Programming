e920d3a512206631caa48b888e012582e7689581
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-13 13:05:31 +0100

    Good practice

--- Exam Preparation/10 - Arrays.cpp
+++ Exam Preparation/10 - Arrays.cpp
@@ -8,5 +8,14 @@ using namespace std;
 
 int main ()
 {
+    const int number_of_numbers = 10; // Is good practice
+    int numbers[number_of_numbers];
+
+    numbers[1] = 0;
+    numbers[8] = 65165;
+    for (auto number : numbers) {
+        cout << number << endl;
+    }
+    
     return 0;
 }
