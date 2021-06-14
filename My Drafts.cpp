8ef1ad0a776f753bf5431ecdf562349740837dba
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 15:28:27 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -8906,3 +8906,30 @@ int main ()
     using_CPP();
     return 0;
 }
+
+struct FEUP {
+
+    string location;
+    int students;
+    double price;
+};
+
+int main ()
+{   
+    // Normal
+    FEUP faculty;
+    faculty.location = "Rua Torcato Gomes";
+    faculty.students = 4300;
+    faculty.price = 697.00;
+    cout << faculty.location << " " << faculty.price << " " << faculty.students << endl;
+
+    // Using pointers
+    FEUP * f;
+    f = (FEUP *) malloc(sizeof(FEUP));
+    f->location = "Rua da Silva";
+    f->students = 3287;
+    f->price = 700;
+    cout << f->price << " " << f->price << " " << f->price << endl; // Note: *p.location make an error!
+    free(f);
+    return 0;
+}
