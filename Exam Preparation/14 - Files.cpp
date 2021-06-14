9acadded19ea5de436c8e897bc133967efb8feef
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 17:33:58 +0100

    Using files

--- Exam Preparation/14 - Files.cpp
+++ Exam Preparation/14 - Files.cpp
@@ -4,9 +4,26 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
+void showNotes () {
+
+    string file_name = "Notes.txt";
+    ifstream file;
+    file.open(file_name);
+    if (file.good()) {
+        string current_line;
+        while (!file.eof()) {
+            getline(file, current_line);
+            cout << current_line << endl;
+        }
+    }
+    file.close();
+}
+
 int main()
-{
+{   
+    showNotes();
     return 0;
 }
