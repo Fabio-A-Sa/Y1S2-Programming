de2a446a8bd522295271aaa8a3719428661a17a4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 19:16:16 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -9046,3 +9046,40 @@ int main ()
 
     return 0;
 }
+
+void writeBinary() {
+
+    ofstream file;
+    string file_name = "Numbers.dat";
+    file.open(file_name);
+    for (int i = 65 ; i < 65 + 25 ; i++ ) {
+        file << (char) i << endl;
+    }
+    cout << "Exit success!" << endl;
+}
+
+void showNotes () {
+
+    string file_name = "Notes.txt"; // Note: does not necessarily have the extension .txt
+    ifstream file;
+    file.open(file_name);
+    if (file.good()) {
+        string current_line;
+        while (!file.eof()) {
+            getline(file, current_line);
+            cout << current_line << endl;
+        }
+    }
+    else {
+        cerr << "This file not exists!" << endl;
+        exit(1);
+    }
+    file.close();
+}
+
+int main()
+{   
+    showNotes();
+    writeBinary();
+    return 0;
+}
