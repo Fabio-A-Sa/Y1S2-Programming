3650ee9da875bd7efb3e95505f3645b27afc7b8e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 19:08:11 +0100

    Added formate

--- Exam Preparation/15 - Classes.cpp
+++ Exam Preparation/15 - Classes.cpp
@@ -3,9 +3,42 @@
 // Page 178 of Lectures Material
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+class Person {
+    
+    public:
+        Person();
+        Person(int age, char gender, string faculty, string name);
+        void setAge(int age);
+        void setGender(char gender);
+        void setFaculty(string faculty);
+        void setName(string name);
+        int getAge();
+        char getGender();
+        string getFaculty();
+        string getName();
+
+    private:
+        int age;
+        char gender;
+        string faculty;
+        string name;
+};
+
+Person::Person() {
+    age = 0;
+    gender = ' ';
+    faculty = "";
+    name = "";
+}
+
+Person::Person(int age, char gender,)
+
 int main ()
-{
+{   
+    Person P = Person(18, 'M', "FEUP", "Fabio");
+    cout << p.getName();
     return 0;
 }
