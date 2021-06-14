f748a3c31ce10babbd401f64d66cd57c1607d64b
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 15:27:09 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -8665,3 +8665,59 @@ int main()
     return 0;
 }
 
+int sum (int number1, int number2) { return number1 + number2 ; }
+
+int main()
+{
+    // Y is memory-copy of X. If X is changed, Y is changed too.
+    int x;
+    int &y = x;
+    x = 10;
+    cout << "x = " << x << "; y = " << y << endl;
+    y = 205654;
+    cout << "x = " << x << "; y = " << y << endl;
+    x = 10;
+    cout << "x = " << x << "; y = " << y << endl;
+
+    // Pointer operators
+    // & --> Returns the memory adress of its operand
+    int example = 10;
+    int *p1 = &example;
+    cout << "& example: " << p1 << endl;
+    
+    // * --> Returns the value of number in adress *p1
+    int another = *p1;
+    cout << another << endl;
+
+    // Pointers and arrays:
+    int arr[] = {10, 52, -9, 56, 23, 7, 23};
+    int *pointer = arr;
+    for (int i = 0 ; i < sizeof(arr) / sizeof(arr[0]) ; i++ ) {
+        cout << *(pointer+i) << " ";
+    }
+    cout << endl;
+
+    // Pointers and functions:
+    int (*p_function) (int, int) = sum;
+    int result = (*p_function) (2, 3);
+    cout << "Sum with pointer function: " << result << endl;
+
+    return 0;
+}
+
+int main ()
+{   
+    srand(time(NULL));
+    int counter = 0;
+    int min = 100, max = 102;
+
+    while (counter != 10) {
+        cout << min + rand() % (max - min + 1) << endl;
+        counter++; 
+    }
+    cout << "Random: " << rand() << endl;
+    cout << "Number of seconds after 1 January 1970: " << time(NULL) << endl;
+
+    return 0;
+}
+
