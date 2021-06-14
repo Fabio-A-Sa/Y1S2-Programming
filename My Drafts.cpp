28b92051f8af3a56e9e9ee99f31f5ff9e3b2800f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 19:16:29 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -9083,3 +9083,55 @@ int main()
     writeBinary();
     return 0;
 }
+
+class Person {
+    
+    public:
+        Person();
+        Person(int age, char gender, string faculty, string name);
+        void setAge(int age);
+        void setGender(char gender);
+        void setFaculty(string faculty);
+        void setName(string name);
+        int getAge();
+        char getGender();
+        string getFaculty();
+        string getName();
+
+    private:
+        int age;
+        char gender;
+        string faculty;
+        string name;
+};
+
+Person::Person() {
+    age = 0;
+    gender = ' ';
+    faculty = "";
+    name = "";
+}
+
+Person::Person(int age, char gender, string faculty, string name) {
+    this -> age = age;
+    this -> gender = gender;
+    this -> faculty = faculty;
+    this -> name = name;
+}
+
+void Person::setAge(int age) { this -> age = age ; }
+void Person::setGender(char gender) { this -> gender = gender ; }
+void Person::setFaculty(string faculty) { this -> faculty = faculty ; }
+void Person::setName(string name) { this -> name = name ; }
+
+int Person::getAge() { return age; }
+char Person::getGender() { return gender; }
+string Person::getFaculty() { return faculty; }
+string Person::getName() { return name; }
+
+int main ()
+{   
+    Person P = Person(18, 'M', "FEUP", "Fabio");
+    cout << P.getName();
+    return 0;
+}
