1ce0e63a68e07870574c2b5a89f026048d255c86
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 17:27:36 +0100

    Added header

---
+++ Exam Preparation/14 - Files.cpp
@@ -0,0 +1,12 @@
+// Created on June, 2021
+// @author: Fábio Araújo de Sá
+// Page 153 of Lectures Material
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
