27be71cfdf81c94ce28a929803cab4316293e345
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-14 15:28:14 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -8819,3 +8819,90 @@ int main ()
     
     return 0;
 }
+
+void using_C () {
+
+    // 1D array
+    int n = 25, *p;
+    p = (int *) malloc(n * sizeof(int));
+    for (int i = 0 ; i < n ; i++ ) {
+        *(p+i) = i;
+    }
+    for (int i = 0 ; i < n ; i++ ) {
+        cout << *(p+i) << " ";
+    }
+    cout << endl;
+    free(p);
+
+    // 2D array
+    int ncol = 3, nline = 5, **matrix;
+    matrix = (int **) malloc (nline * sizeof(int *));
+    for (int i = 0 ; i < nline ; i++ ) {
+        matrix[i] = (int *) malloc (ncol * sizeof(int));
+    }
+
+    for (int i = 0 ; i < nline ; i++) {
+        for (int j = 0 ; j < ncol ; j++) {
+            matrix[i][j] = i+j;
+        }
+    }
+    
+    for (int i = 0 ; i < nline ; i++) {
+        for (int j = 0 ; j < ncol ; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    free(matrix);
+}
+
+void using_CPP () {
+
+    // 1D array
+    int n = 25;
+    int *p = new int[n];
+    
+    for (int i = 0 ; i < n ; i++ ) {
+        *(p+i) = i;
+    }
+
+    for (int i = 0 ; i < n ; i++ ) {
+        cout << *(p+i) << " ";
+    }
+    cout << endl;
+    delete [] p;
+
+    // 2D array
+    int nc = 10, nl = 20, **matrix;
+    matrix = new int*[nl];
+    for (int i = 0 ; i < nl ; i++ ) {
+        matrix[i] = new int[nc];
+    }
+
+    for (int i = 0 ; i < nl ; i++ ) {
+        for (int j = 0 ; j < nc ; j++ ) {
+            matrix[i][j] = i+j;
+        }
+    }
+
+    for (int i = 0 ; i < nl ; i++ ) {
+        for (int j = 0 ; j < nc ; j++ ) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0 ; i < nl ; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+int main ()
+{
+    // Using C and C++ methods
+    using_C();
+    using_CPP();
+    return 0;
+}
