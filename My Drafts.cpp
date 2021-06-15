65c366e0361b889191d5592d1033c835a7bf8e98
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-16 00:04:03 +0100

    Drafts update

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -9135,3 +9135,26 @@ int main ()
     cout << P.getName();
     return 0;
 }
+
+void using_iterators() {
+    
+    vector<string> names = {"Marco", "Ilias", "Aurelio", "Tobias", "Crespo", "Vint"};
+
+    // Construção de um apontador próprio para vectores de strings
+    vector<string>::iterator n;                       
+
+    // names.begin() aponta para o primeiro valor do container
+    // names.end() aponta para a zona de memória seguinte ao último dado contido no container
+    // n recebe, a cada iteração, um incremento de X bytes, correspondentes ao tipo de dados manipulados                          
+    for (n = names.begin() ; n != names.end() ; n++ ) {
+        cout << *n << endl;                                     // Retorna o que encontra na memória                            
+    }
+
+    // Continue in page 253 tomorrow!
+}
+
+int main ()
+{   
+    using_iterators();
+    return 0;
+}
