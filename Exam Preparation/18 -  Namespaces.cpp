dded76eace89ebeb94b21e25c756c6fced385fb4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-16 18:48:05 +0100

    Added code

--- Exam Preparation/18 -  Namespaces.cpp
+++ Exam Preparation/18 -  Namespaces.cpp
@@ -3,8 +3,34 @@
 // Page 330 of Lectures Material
 
 #include <iostream>
-using namespace std;
+#include <string>
+// using namespace std;
 
-namespace my {
-    
+namespace myfuncs {
+    void cout(const std::string &s);
+    bool endl(const std::string &s);
+}
+
+void myfuncs::cout(const std::string &s) {
+    int i = 0;
+    while (s[i]) {
+        if (isalpha(s[i])) {
+            std::cout << s[i];
+        }
+        i++;
+    }
+    std::cout << std::endl;
+}
+
+bool myfuncs::endl(const std::string &s) {
+    return islower(s[s.size()-1]);
+}
+
+int main ()
+{
+    std::string test = "Th78392is0is5627a7t892es0t3to5Main0Fun82737c2837tio327n";
+    myfuncs::cout(test);
+    std::string answer = myfuncs::endl(test) ? "Lower case" : "Upper case";
+    std::cout << answer << std::endl;
+    return 0;
 }
