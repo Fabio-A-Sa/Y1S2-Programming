d8f113e2c788072242068166fd7f7c3170e11bc4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-16 13:55:32 +0100

    Corrections

--- Exam Preparation/17 - STL Algorithms.cpp
+++ Exam Preparation/17 - STL Algorithms.cpp
@@ -111,7 +111,7 @@ void STL_lists() {
     show_lists(l, l.size());
 
     // Insert algorithm --> In vectors is more easy!
-    list<int>::iterator P;
+    list<int>::iterator P = l.begin();
     int index = 5;
     int value = 541;
     for (int i = 0 ; i < index ; i++ ) { P++; }
