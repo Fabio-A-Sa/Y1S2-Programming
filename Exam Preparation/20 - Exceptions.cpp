809c7809c0c898041b25fc05f78eea5a955fe628
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-16 21:11:42 +0100

    Added errors

--- Exam Preparation/20 - Exceptions.cpp
+++ Exam Preparation/20 - Exceptions.cpp
@@ -7,5 +7,14 @@ using namespace std;
 
 int main ()
 {   
+    int number = 10;
+    int zero = 0;
+    try { 
+        int result = number / zero ; 
+        cout << "The result is : " << result << endl ; 
+    }
+    catch (...) { 
+        cout << "Divide by zero error!" << endl; 
+    }
     return 0;
 }
