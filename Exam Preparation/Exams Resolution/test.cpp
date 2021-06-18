96530fb3eba4892014c5ee14f27f01e93f83da2a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-18 19:21:23 +0100

    Test

--- Exam Preparation/Exams Resolution/test.cpp
+++ Exam Preparation/Exams Resolution/test.cpp
@@ -9,6 +9,12 @@ class Date {
         int i, j, k;
 };
 
+Date::Date(int i, int j, int k) {
+    this->i = i;
+    this->k = k;
+    this->j = j;
+}
+
 class Novo {
     public:
         Novo(int i= 3, int j= 8, int k= 9);
@@ -16,8 +22,14 @@ class Novo {
         int i, j, k ;
 };
 
+Novo::Novo(int i, int j, int k) {
+    this->i = i;
+    this->k = k;
+    this->j = j;
+}
+
 int main ()
 {
-    Novo n;
+    Date d;
     return 0;
 }
