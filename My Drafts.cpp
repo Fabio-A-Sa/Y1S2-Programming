659893287eaedb5b33a8bae43ef1c4e994b97d26
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-18 19:34:53 +0100

    Added exam

--- My Drafts.cpp
+++ My Drafts.cpp
@@ -12160,3 +12160,39 @@ A função draw() é declarada como virtual, está presente unicamente na Parent
 objectos de tipo Shape, vector<Shape*>), quando se coloca a instrução item.draw(), o compilador, em tempo de execução, irá tratar de
 invocar o método da classe derivada que manipula. As virtual functions servem essencialmente para declarar métodos a serem implementados,
 de facto, só nas funções derivadas (Child Classes).
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date {
+    public:
+        Date(int i, int j, int k);
+    private:
+        int i, j, k;
+};
+
+Date::Date(int i, int j, int k) {
+    this->i = i;
+    this->k = k;
+    this->j = j;
+}
+
+class Novo {
+    public:
+        Novo(int i= 3, int j= 8, int k= 9);
+    private:
+        int i, j, k ;
+};
+
+Novo::Novo(int i, int j, int k) {
+    this->i = i;
+    this->k = k;
+    this->j = j;
+}
+
+int main ()
+{
+    Date d;
+    return 0;
+}
